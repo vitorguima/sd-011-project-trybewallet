5558fe3570e2579b31181b2fd84219b15c506131
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2021-07-07 18:31:58 -0300

    adicionada funcao de fetch e funcoes de sucesso e erro da api

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,3 +4,20 @@ const sendEmail = (payload) => ({
 });
 
 export default sendEmail;
+
+const requestCoinsSucess = (payload) => ({
+  type: 'REQUEST_SUCESS',
+  payload,
+});
+
+const requestCoinsError = (payload) => ({
+  type: 'REQUEST_ERROR',
+  payload,
+});
+
+export const fetchCoins = () => (dispatch) => {
+  fetch('https://economia.awesomeapi.com.br/json/all')
+    .then((result) => result.json())
+    .then((data) => dispatch(requestCoinsSucess(data)))
+    .catch((error) => dispatch(requestCoinsError(error.message)));
+};
