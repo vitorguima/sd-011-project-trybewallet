1acc6b75eb1f91149550b5657621477ab1402395
Marconi <marconieng@gmail.com>
2021-07-07 18:54:23 -0300

    Adiciona requisitos 2 e 3

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,29 +1,92 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router';
+import PropTypes from 'prop-types';
+import { userInfo } from '../actions';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.handleChange = this.handleChange.bind(this);
+    this.inputsValidate = this.inputsValidate.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      disabled: true,
+      redirect: false,
+      email: '',
+      password: 0,
+    };
+  }
+
+  handleChange({ target: { name, value } }) {
+    this.setState({
+      [name]: value,
+    }, this.inputsValidate);
+  }
+
+  inputsValidate() {
+    const { email, password } = this.state;
+    const PASSWORD_LENGTH = 6;
+    const regexEmail = /\S+@\S+\.\S+/;
+    const emailText = email;
+    const passwordNumbers = password;
+    if (regexEmail.test(emailText) && passwordNumbers.length >= PASSWORD_LENGTH) {
+      this.setState({ disabled: false });
+    } else {
+      this.setState({ disabled: true });
+    }
+  }
+
+  handleClick() {
+    const { email } = this.state;
+    const { userToSend } = this.props;
+    this.setState({ redirect: true });
+    userToSend(email);
+  }
+
   render() {
+    const { disabled, redirect } = this.state;
+    if (redirect) {
+      return <Redirect to="/carteira" />;
+    }
     return (
       <main>
         <h1>Trybe Wallet</h1>
         <label htmlFor="email-input">
           <input
-            type="text"
-            name="name"
+            type="email"
+            name="email"
             data-testid="email-input"
             placeholder="Digite seu e-mail"
+            onChange={ (event) => this.handleChange(event) }
           />
         </label>
         <label htmlFor="password-input">
           <input
-            type="text"
+            type="password"
             name="password"
             data-testid="password-input"
             placeholder="Digite sua senha"
+            onChange={ (event) => this.handleChange(event) }
           />
         </label>
-        <button type="button">Entrar</button>
+        <button
+          type="button"
+          disabled={ disabled }
+          onClick={ this.handleClick }
+        >
+          Entrar
+        </button>
       </main>);
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  userToSend: (email) => dispatch(userInfo(email)),
+});
+
+Login.propTypes = {
+  userToSend: PropTypes.func,
+}.isRequired;
+
+export default connect(null, mapDispatchToProps)(Login);
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,25 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+
+const INITIAL_STATE = {
+  currencies: [],
+  expenses: [],
+};
+
+function wallet(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case 'CURRENCIES_VALUES':
+    return {
+      ...state,
+      currencies: action.state,
+    };
+  case 'EXPENSES_VALUES':
+    return {
+      ...state,
+      expenses: [...state.expenses, action.state],
+    };
+  default:
+    return state;
+  }
+}
+
+export default wallet;
