de71c56fdffb77d7e51dd2df79bdf61c86982983
Matheus_Alexandre <alexandremhm@gmail.com>
2021-07-07 14:57:00 -0300

    Add pagina de login com inputs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Login from './pages/Login';
 
-function App() {
-  return <div>Hello, TrybeWallet!</div>;
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Route exact path="/" component={ Login } />
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <input type="text" placeholder="Digite seu Email" data-testid="email-input" />
+        <input type="text" placeholder="Digite sua senha" data-testid="password-input" />
+        <button type="submit">Entrar</button>
+      </div>);
   }
 }
 
