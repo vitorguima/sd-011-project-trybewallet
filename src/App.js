399b5750faf5cfb58b39214529cb551f7f4f45e8
Luiz Otavio <luiz.otavio273@gmail.com>
2021-07-07 19:34:00 -0300

    req 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import Login from './pages/Login';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <>
+      <h1>Hello, TrybeWallet!</h1>
+      <switch>
+        <Route exact path="/" component={ Login } />
+      </switch>
+    </>
+  );
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,31 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <form>
+        <label htmlFor="email-input">
+          <input
+            data-testid="email-input"
+            type="email"
+            name="email"
+            placeholder="Email"
+          />
+        </label>
+        <label htmlFor="password-input">
+          <input
+            data-testid="password-input"
+            type="password"
+            name="pass"
+            placeholder="Senha"
+          />
+        </label>
+        <button
+          type="button"
+        >
+          Entrar
+        </button>
+      </form>
+    );
   }
 }
 
