2627a39db3becf663049fd4ca69711a26811af2f
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2021-07-07 18:08:12 -0300

    adicionando input em todos os inputs e select

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -6,21 +6,21 @@ export default class Form extends Component {
       <form>
         <label htmlFor="value">
           Valor
-          <input type="text" name="value" />
+          <input id="value" type="text" name="value" />
         </label>
         <label htmlFor="description">
           Descrição
-          <input type="text" name="description" />
+          <input id="description" type="text" name="description" />
         </label>
         <label htmlFor="coin">
           Moeda
-          <select name="coin">
+          <select id="coin" name="coin">
             <option>0</option>
           </select>
         </label>
         <label htmlFor="payment-method">
           Método de pagamento
-          <select name="payment-method">
+          <select id="payment-method" name="payment-method">
             <option value="money">Dinheiro</option>
             <option value="credit-card">Cartão de crédito</option>
             <option value="debit-card">Cartão de débito</option>
@@ -28,7 +28,7 @@ export default class Form extends Component {
         </label>
         <label htmlFor="tag">
           Tag
-          <select name="tag">
+          <select id="tag" name="tag">
             <option value="food">Alimentação</option>
             <option value="leisure">Lazer</option>
             <option value="work">Trabalho</option>
