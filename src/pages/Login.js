eece46bf17e6cd8df6f75d11e01269e3dfa460d4
thosijulio <thosijulio@gmail.com>
2021-07-07 15:39:17 -0300

    resolucao exercicio 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,16 +1,48 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+    this.handleForm = this.handleForm.bind(this);
+  }
+
+  handleForm({ target: { id, value } }) {
+    this.setState((oldState) => ({
+      ...oldState,
+      [id]: value,
+    }));
+  }
+
   render() {
+    const { email, password } = this.state;
+
     return (
       <form>
         <label htmlFor="email">
           Email:
-          <input type="email" data-testid="email-input" id="email" required />
+          <input
+            type="email"
+            data-testid="email-input"
+            id="email"
+            value={ email }
+            onChange={ this.handleForm }
+            required
+          />
         </label>
         <label htmlFor="password">
           Senha:
-          <input type="password" data-testid="password-input" id="password" required />
+          <input
+            type="password"
+            data-testid="password-input"
+            id="password"
+            value={ password }
+            onChange={ this.handleForm }
+            required
+          />
         </label>
         <button type="button">Enviar</button>
       </form>
