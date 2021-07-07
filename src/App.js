a9004809c06d87907456cec255e27539f74428ad
josanjohnata <josanjohnata@gmail.com>
2021-07-07 15:32:43 -0300

    Adiciona p user, wallet, store e a rota

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
-import { BrowserRouter, Provider } from 'react-router-dom'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Provider>
-        <Login />
-      </Provider>
-    </BrowserRouter>
+    <div>
+      <Switch>
+        <Route to path="/" component={ Login } />
+      </Switch>
+    </div>
   )
 }
 
