a59e1b45f4fd8a8ee29b60fe87e95c7ad4a65fd3
Tales Coelho <tales-coelho@live.com>
2021-07-07 13:28:31 -0300

    Criando a estrutura do store

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,14 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+const INITIAL_STATE = {
+  name: '',
+};
+
+const wallet = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case 'TESTE':
+    return state;
+  default:
+    return state;
+  }
+};
+
+export default wallet;
---
+++ src/store/index.js
@@ -0,0 +1,7 @@
+import { createStore, compose } from 'redux';
+import rootReducer from '../reducers';
+
+const extension = window.devToolsExtension() || ((f) => f);
+const store = createStore(rootReducer, compose(extension));
+
+export default store;
