62090799fa2370297344fe331566cf5b505d58c3
Luciano Pimenta <l.a.p.pimenta.j@gmail.com>
2021-07-07 14:57:35 -0300

    Requisito 1 - Concluido

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,29 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <label htmlFor="email">
+          E-mail:
+          <input
+            data-testid="email-input"
+            placeholder="Insira seu e-mail"
+            id="email"
+            type="email"
+          />
+        </label>
+        <label htmlFor="password">
+          Senha:
+          <input
+            data-testid="password-input"
+            placeholder="Insira sua senha"
+            id="password"
+            type="password"
+          />
+        </label>
+        <button type="button">Entrar</button>
+      </div>
+    );
   }
 }
 
