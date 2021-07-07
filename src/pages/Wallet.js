db6a956735123be6aa95b80bd9b68b640cfce461
eraldoalmeida <eraldo.salmeida@gmail.com>
2021-07-07 16:12:47 -0300

    requisito 6

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../components/Header';
+import FormExpanses from '../components/FormExpanses';
 
 class Wallet extends React.Component {
   render() {
     return (
       <div>
         <Header />
+        <FormExpanses />
       </div>
     );
   }
