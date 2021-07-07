9ee4c2a96c4fbb69391a05dce68f90b1c19ac2e3
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2021-07-07 17:23:04 -0300

    adicionando h4 para mostrar o cambio utilizado

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -7,6 +7,7 @@ class Wallet extends React.Component {
         <h1>TrybeWallet</h1>
         <h4 data-testid="email-field">{`Bem vindo: ${ email }`}</h4>
         <h4 data-testid="total-field">0</h4>
+        <h4 data-testid="header-currency-field">BRL</h4>
       </header>
     );
   }
