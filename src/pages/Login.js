be1f8c37099cb8fc034095f14bfcd40789a28729
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-07-07 16:18:10 -0300

    Requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,27 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div className="login-area">
+        <label htmlFor="email">
+          <input
+            type="text"
+            name="email"
+            data-testid="email-input"
+            placeholder="Digite seu email"
+          />
+        </label>
+        <label htmlFor="password">
+          <input
+            type="password"
+            name="password"
+            data-testid="password-input"
+            placeholder="Insira sua senha"
+          />
+        </label>
+        <div role="button">Entrar</div>
+      </div>
+    );
   }
 }
 
