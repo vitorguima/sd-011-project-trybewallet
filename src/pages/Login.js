d34e7d42f6d749c4cc834e1ae5ceaf04dc160382
MatheusAraujoDev <matheusaraujo.dev@gmail.com>
2021-07-07 15:17:48 -0300

    Adiciona requisito 01

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,19 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <label htmlFor="input-email" data-testid="email-input">
+          Email:
+          <input />
+        </label>
+        <label htmlFor="input-password" data-testid="password-input">
+          Senha:
+          <input />
+        </label>
+        <button type="button">Entrar</button>
+      </div>
+    );
   }
 }
 
