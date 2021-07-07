a53769a3470abca1f27a9fa3a5aebc1af19a6d92
Reginara Gomes <refagomes@gmail.com>
2021-07-07 20:51:11 -0300

    Adiciona configuraçoes iniciais de Redux.

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,19 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_STATE = {
+  user: {
+    email: '',
+  },
+};
+
+const userReducer = (state = INITIAL_STATE, action) => {
+  switch (actioin.type) {
+    case 'Login':
+      return {
+        ...state,
+        users: { email: action.payload },
+      };
+    default:
+      return state;
+  }
+};
+
+export default userReducer;
