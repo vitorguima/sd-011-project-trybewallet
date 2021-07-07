cbaa3efdf1236b4871dc198616e712cb9cbec309
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-07-07 14:53:13 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Login from './pages/Login';
 
-function App() {
-  return <div>Hello, TrybeWallets!</div>;
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+        </Switch>
+      </div>
+    );
+  }
 }
-
-export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,20 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        Login
+        <label htmlFor="email">
+          <input type="text" placeholder="Email" data-testid="email-input" />
+        </label>
+        <label htmlFor="password">
+          <input type="password" placeholder="Senha" data-testid="password-input" />
+        </label>
+        <button type="button">
+          Entrar
+        </button>
+      </div>
+    );
   }
 }
 
