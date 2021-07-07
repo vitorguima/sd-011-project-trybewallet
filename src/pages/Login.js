bd473886216d123ca205ec16f24575168463838f
Daniel <danielfdoficial@gmail.com>
2021-07-07 15:45:27 -0300

    primeiro requisito sem lint

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,33 @@
 import React from 'react';
+import '../CSS/Login.css';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div className="Login-Page">
+        <h1>The Wallet - Login</h1>
+        <form className="login-form">
+          <label htmlFor="email-input">
+            <input
+              type="text"
+              name="email-input"
+              className="email-input"
+              placeholder="insira aqui o seu email"
+              data-testid="email-input"
+            />
+          </label>
+          <label htmlFor="password-input">
+            <input
+              type="password"
+              name="password-input"
+              className="password-input"
+              placeholder="insira aqui a sua senha"
+              data-testid="password-input"
+            />
+          </label>
+          <button type="button" className="Login-Button">Entrar</button>
+        </form>
+      </div>);
   }
 }
 
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,6 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_STATE = {
+  user: {
+    email: '',
+  },
+};
