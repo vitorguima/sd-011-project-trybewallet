6593a573868e4dfaa1de8e886237d32229da34b9
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2021-07-07 16:51:16 -0300

    criando o userReducer e importando o provider e a store no index.js

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -4,3 +4,19 @@ import SEND_EMAIL from '../actions/index';
 const INITIAL_STATE = {
   email: '',
 };
+
+function userReducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case SEND_EMAIL:
+    return {
+      ...state,
+      email: action.payload,
+    };
+  default:
+    return {
+      ...state,
+    };
+  }
+}
+
+export default userReducer;
