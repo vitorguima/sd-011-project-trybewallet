a0a5643fbd1fe7399dba2b2fc1fcbd037ba2b0e3
Vitor <vitorguima@gmail.com>
2021-07-07 15:10:20 -0300

    requisito 1 finalizado

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Login from './pages/Login';
+
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,51 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState(() => ({
+      [name]: value,
+    }));
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+
+    return (
+      <div className="login-wrapper">
+        <form>
+          <input
+            type="email"
+            placeholder="Email"
+            data-testid="email-input"
+            value={ email }
+            onChange={ this.handleChange }
+          />
+          <input
+            type="password"
+            placeholder="Senha"
+            data-testid="password-input"
+            value={ password }
+            onChange={ this.handleChange }
+          />
+        </form>
+        <button
+          type="button"
+        >
+          Entrar
+        </button>
+      </div>
+    );
   }
 }
 
