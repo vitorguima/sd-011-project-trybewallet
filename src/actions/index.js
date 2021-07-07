6b13ca80b61a6b67a170060d168a6b9979f38c7a
Oryange <oryangestrifezze@gmail.com>
2021-07-07 14:13:02 -0300

    Inicia o projeto

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,7 @@
-// Coloque aqui suas actions
+export default function userAction(email, senha) {
+  return ({
+    type: 'INPUT_USER',
+    email,
+    senha,
+  });
+}
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,16 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_STATE = {
+  email: '',
+  senha: '',
+};
+
+export default function reducerUser(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case 'INPUT_USER':
+    return ({
+      email: action.email,
+      senha: action.senha,
+    });
+  default:
+    return state;
+  }
+}
