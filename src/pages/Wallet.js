f6eee0a0d55aea5e9c30aa4ea014e1d668f8d6b6
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2021-07-07 18:02:20 -0300

    criando outro componente para o formulario e importando ele no wallet

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import Form from './Form';
 
 class Wallet extends React.Component {
   render() {
@@ -13,42 +14,8 @@ class Wallet extends React.Component {
           <h4 data-testid="total-field">0</h4>
           <h4 data-testid="header-currency-field">BRL</h4>
         </header>
-
         <section className="form">
-          <form>
-            <label htmlFor="value">
-              Valor
-              <input type="text" name="value" />
-            </label>
-            <label htmlFor="description">
-              Descrição
-              <input type="text" name="description" />
-            </label>
-            <label htmlFor="coin">
-              Moeda
-              <select name="coin">
-                <option></option>
-              </select>
-            </label>
-            <label htmlFor="payment-method">
-              Método de pagamento
-              <select name="payment-method">
-                <option value="money">Dinheiro</option>
-                <option value="credit-card">Cartão de crédito</option>
-                <option value="credit-card">Cartão de débito</option>
-              </select>
-            </label>
-            <label htmlFor="tag">
-              Tag
-              <select name="tag">
-                <option value="food">Alimentação</option>
-                <option value="leisure">Lazer</option>
-                <option value="work">Trabalho</option>
-                <option value="transport">Transporte</option>
-                <option value="health">Saúde</option>
-              </select>
-            </label>
-          </form>
+          <Form />
         </section>
       </main>
     );
