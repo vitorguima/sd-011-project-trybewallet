c4e5cdfa7f687b914350077ceb0be84319d8171e
Marcelo <marsleite@gmail.com>
2021-07-07 14:42:36 -0300

    iniciando com tudo, vamos que vamos :rocket:

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-class Login extends React.Component {
-  render() {
-    return <div>Login</div>;
-  }
-}
+export default function Login() {
+  return (
+    <div>
 
-export default Login;
+    </div>
+  )
+}
