0024268234657d92f7b94a9de383e5f599dd71a1
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2021-07-07 15:41:25 -0300

    configurando requisito 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import Login from './pages/Login';
+// import './App.css';
+// import { connect } from 'react-redux';
+// import { fetchPokemon } from './actions/index.js';
 
-function App() {
-  return <div>Hello, TrybeWallet!</div>;
+class App extends Component {
+  // constructor(props) {
+  //   super(props)
+  // }
+
+  render() {
+    return (
+      <div>
+        <Route exact path="/" component={ Login } />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,27 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <label htmlFor="email" data-testid="email-input">
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Email"
+          />
+        </label>
+        <label htmlFor="senha" data-testid="password-input">
+          <input
+            type="password"
+            name="senha"
+            id="senha"
+            placeholder="Senha"
+          />
+        </label>
+        <button type="button">Entrar</button>
+      </div>
+    );
   }
 }
 
