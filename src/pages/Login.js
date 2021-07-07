4cd896a79dc9a35a4ae8f815cda86f2f08638673
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2021-07-07 15:58:13 -0300

    criando constructor e state

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 
 export default class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
   render() {
     return (
       <div>
