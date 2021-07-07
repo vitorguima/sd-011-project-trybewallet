2be7b8ba73eba6f5ab77bdbe49fc629c68be5213
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2021-07-07 16:05:40 -0300

    atribuindo name, value e funcao onchange para os inputs

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -19,16 +19,23 @@ export default class Login extends Component {
   }
 
   render() {
+    const { email, password } = this.state;
     return (
       <div>
         <input
           data-testid="email-input"
           type="email"
+          onChange={ this.handleChange }
+          name="email"
+          value={ email }
           placeholder="Digite seu email"
         />
         <input
           data-testid="password-input"
+          onChange={ this.handleChange }
           type="password"
+          name="password"
+          value={ password }
           placeholder="Digite sua senha"
         />
         <button type="button">
