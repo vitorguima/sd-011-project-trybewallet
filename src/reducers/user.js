becb4012e8807e16c566e918b73d805ca6d2752a
Diego Ribeiro <dihrm2001@gmail.com>
2021-07-07 15:34:27 -0300

    Requisito 3

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,9 +1,15 @@
+import { LOG_USER } from '../actions';
+
 const INITIAL_STATE = {
   email: '',
 };
 
-const user = (state = INITIAL_STATE, action) => {
-  switch (action.type) {
+const user = (state = INITIAL_STATE, { type, payload }) => {
+  switch (type) {
+  case LOG_USER:
+    return {
+      email: payload,
+    };
   default:
     return state;
   }
