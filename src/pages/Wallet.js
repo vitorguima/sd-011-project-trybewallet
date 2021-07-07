4e44a03fbdaca1e2c9e74d6ce176f11013cc1308
¨Hugosomers¨ <hugosomersla@gmail.com>
2021-07-07 16:52:15 -0300

    Requisito 5 feito

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import './Wallet.css';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { email } = this.props;
+    return (
+      <div>
+        <header className="walletHeader">
+          <h1>TrybeWallet</h1>
+          <h3>
+            Seja bem vindo:&nbsp;
+            <span data-testid="email-field">{email}</span>
+          </h3>
+          <h5 data-testid="total-field">
+            Despesa total: 0&nbsp;
+            <span data-testid="header-currency-field">BRL</span>
+          </h5>
+        </header>
+      </div>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+export default connect(mapStateToProps)(Wallet);
+
+Wallet.propTypes = {
+  email: PropTypes.string.isRequired,
+};
