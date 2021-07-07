764fc519d448c21f8503d1c557d29eaf7bdc7257
Ghislaine <glenox79@gmail.com>
2021-07-07 17:44:53 -0300

    Creating login home page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,45 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      email: '',
+      password: '',
+    }
+  }
+
+  handleChangeInput({ target }) {
+this.setState({
+  [target.name]: target.value,
+})
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    return (
+      <form>
+        <input
+          data-testid="email-input"
+          type="email"
+          placeholder="Informe seu email"
+          name="email"
+          value={email}
+          onChange={this.handleChangeInput}
+        />
+        <input
+          data-testid="password-input"
+          type="password"
+          placeholder="Informe sua senha"
+          name="password"
+          value={password}
+          onChange={this.handleChangeInput}
+        />
+        <button type="button">
+          Entrar
+        </button>
+      </form>
+    );
   }
 }
 
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,22 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_STATE = {
+  user: {
+    email: '',
+  },
+  wallet: {
+    currencies: [],
+    expenses: []
+  }
+};
+
+function user(state = INITIAL_STATE, action) {
+  switch (action.type) {
+    case 'ACTION':
+      return state;
+
+    default:
+      return state;
+  }
+}
+
+export default user;
