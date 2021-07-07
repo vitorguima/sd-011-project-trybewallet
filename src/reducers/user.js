3e734911271bb533dbaffa91a79aa813ef04c016
IeiriMauricio <mauricio.s.ieiri@gmail.com>
2021-07-07 14:52:18 -0300

    inicio do projeto

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,21 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_STATE = {
+  user: {
+    email: '',
+  },
+};
+
+const user = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case 'LOGIN':
+    return {
+      user: {
+        email: action.email,
+      },
+    };
+  default:
+    return state;
+  }
+};
+
+export default user;
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,32 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+
+const INITIAL_STATE = {
+  wallet: {
+    currencies: [],
+    expenses: [],
+  },
+};
+
+const user = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case 'WALLET_ADD':
+    return {
+      wallet: {
+        currencies: action.currencies,
+        expenses: action.expenses,
+      },
+    };
+  case 'WALLET_SUB':
+    return {
+      wallet: {
+        ...state,
+        currencies: action.currencies,
+        expenses: state.expenses - action.expenses,
+      },
+    };
+  default:
+    return state;
+  }
+};
+
+export default user;
