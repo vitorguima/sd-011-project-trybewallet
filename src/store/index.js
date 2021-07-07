1e7908dc67a06d84603462c48adef1f055c57338
Gisele Costa <jikinha@gmail.com>
2021-07-07 16:25:39 -0300

    requisito 1

---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+import { createStore, compose, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import reducer from '../reducers';
+
+const extension = window.devToolsExtension() || ((f) => f);
+
+const store = createStore(
+  reducer,
+  composeWithDevTools(
+    applyMiddleware(thunk),
+  ),
+  compose(extension)
+);
+
+export default store;
