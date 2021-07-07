8c5a913e3647f6205340b9e4e2a57ab4e18542c3
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-07-07 16:49:58 -0300

    Adiciona requisito 1 falhando no avaliador

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,41 @@
 import React from 'react';
+import 'bulma/css/bulma.min.css';
+import './Login.css';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <section className="login">
+        <h1> Sistema Trybe Wallet </h1>
+        <div className="field">
+          <p className="control has-icons-left has-icons-right">
+            <input
+              className="input"
+              type="email"
+              placeholder="E-mail"
+              data-testid="email-input"
+            />
+          </p>
+        </div>
+        <div className="field">
+          <p className="control has-icons-left">
+            <input
+              className="input"
+              type="password"
+              placeholder="Password"
+              data-testid="password-input"
+            />
+          </p>
+        </div>
+        <div className="field">
+          <p className="control">
+            <button className="button is-success" type="button">
+              Entrar
+            </button>
+          </p>
+        </div>
+      </section>
+    );
   }
 }
 
