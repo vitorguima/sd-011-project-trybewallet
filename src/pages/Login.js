62000cb69e0e8ba049a9c0f9781d23892da65e5a
¨Hugosomers¨ <hugosomersla@gmail.com>
2021-07-07 15:34:18 -0300

    Requisito 1 feito

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,38 @@
 import React from 'react';
+import './Login.css';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div className="loginContainer">
+
+        <div className="loginHeroDiv">
+          <h1>Login</h1>
+        </div>
+
+        <form className="loginForm">
+          <label htmlFor="email" className="formLabel">
+            <input
+              placeholder="Email"
+              name="email"
+              type="email"
+              data-testid="email-input"
+            />
+          </label>
+
+          <label htmlFor="password" className="formLabel">
+            <input
+              placeholder="Senha"
+              name="password"
+              type="password"
+              data-testid="password-input"
+            />
+          </label>
+          <button type="button">Entrar</button>
+        </form>
+
+      </div>
+    );
   }
 }
 
