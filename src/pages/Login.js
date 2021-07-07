a22524377c615d6f36c1944433a6edd381d63abb
getuliogutemberg <getulio.dev@gmail.com>
2021-07-07 16:49:53 -0300

    requisito 2 ok

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
   render() {
+    const { email, password } = this.state;
+    const emailRegex = /\b[\w.-]+@[\w.-]+\.\w{2,4}\b/gi; // (regex) https://regexr.comm/2ri2c
+    const six = 6;
     return (
       <div>
         <h3>Login</h3>
@@ -11,17 +22,23 @@ class Login extends React.Component {
           data-testid="email-input"
           type="text"
           placeholder="E-mail"
+          onChange={ (event) => this.setState({ email: event.target.value }) }
         />
 
         <input
           data-testid="password-input"
           type="text"
           placeholder="Senha"
+          onChange={ (event) => this.setState({ password: event.target.value }) }
+
         />
 
         <Link to="/carteira">
           <button
             type="submit"
+            disabled={
+              !(email.match(emailRegex)) || password.length < six // validação de email e senha (six por conta do magicNumber)
+            }
           >
             Entrar
           </button>
