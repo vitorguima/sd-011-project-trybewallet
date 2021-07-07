d1bc716c54ef7b63f513792028c45494bfa7cc20
Bruno Duarte <brunoarduarte@gmail.com>
2021-07-07 18:55:54 -0300

    Requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,34 @@
 import React from 'react';
 
-class Login extends React.Component {
-  render() {
-    return <div>Login</div>;
+function Login() {
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  function handleSubmit(event) {
+    event.preventDefaul();
+    console.log(email);
+    console.log(password);
   }
+
+  return (
+    <form onSubmit={ handleSubmit }>
+      <input
+        type="email"
+        style={ { display: 'block' } }
+        data-testid="email-input"
+        placeholder="Email"
+        onChange={ ({ target }) => setEmail(target.value) }
+      />
+      <input
+        type="password"
+        style={ { display: 'block' } }
+        data-testid="password-input"
+        placeholder="Password"
+        onChange={ ({ target }) => setPassword(target.value) }
+      />
+      <button type="button">Entrar</button>
+    </form>
+  );
 }
 
 export default Login;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,18 @@
-// import user from './user';
-// import wallet from './wallet';
+const INITIAL_STATE = {
+  user: {
+    email: '',
+  },
+  wallet: {
+    currencies: [],
+    expenses: [],
+  },
+};
 
-// Configure os seus reducers.
-// ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
+function reducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  default:
+    return state;
+  }
+}
+
+export default reducer;
