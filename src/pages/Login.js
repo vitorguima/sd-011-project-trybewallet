9f086e7edd97f7934a3d1dd7bf1917bfb7cd3910
thosijulio <thosijulio@gmail.com>
2021-07-07 15:12:29 -0300

    resolucao exercicio 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,19 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <form>
+        <label htmlFor="email">
+          Email:
+          <input type="email" data-testid="email-input" id="email" required />
+        </label>
+        <label htmlFor="password">
+          Senha:
+          <input type="password" data-testid="password-input" id="password" required />
+        </label>
+        <button type="button">Enviar</button>
+      </form>
+    );
   }
 }
 
