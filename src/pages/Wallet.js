bc1ceb3001eaeaf6b3fd49bb0a8f83ade8aed324
Thyago Pessoa <flash_thyago@hotmail.com>
2021-07-07 16:40:38 -0300

    Requisito 6

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -13,6 +13,43 @@ class Wallet extends React.Component {
           <p data-testid="total-field">{0}</p>
           <p data-testid="header-currency-field">BRL</p>
         </header>
+
+        <form>
+          <label htmlFor="valor">
+            Valor
+            <input type="text" name="valor" id="valor" />
+          </label>
+          <label htmlFor="descricao">
+            Descrição
+            <input type="text" name="descrição" id="descricao" />
+          </label>
+          <label htmlFor="moeda">
+            Moeda
+            <select id="moeda">
+              <option>BRL</option>
+              <option>USD</option>
+              <option>EUR</option>
+            </select>
+          </label>
+          <label htmlFor="metodo_de_pagamento">
+            Método de pagamento
+            <select id="metodo_de_pagamento">
+              <option>Dinheiro</option>
+              <option>Cartão de crédito</option>
+              <option>Cartão de débito</option>
+            </select>
+          </label>
+          <label htmlFor="tag">
+            Tag
+            <select id="tag">
+              <option>Alimentação</option>
+              <option>Lazer</option>
+              <option>Trabalho</option>
+              <option>Transporte</option>
+              <option>Saúde</option>
+            </select>
+          </label>
+        </form>
       </div>
     );
   }
