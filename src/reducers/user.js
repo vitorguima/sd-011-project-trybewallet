b324d243160a74e9fab05a995f6313455dcc527d
thosijulio <thosijulio@gmail.com>
2021-07-07 14:57:46 -0300

    organizando store e reducers

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,11 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_STATE = {
+  email: '',
+};
+
+function userReducer(state = INITIAL_STATE, action) {
+  console.log(action);
+  return state;
+}
+
+export default userReducer;
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,12 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+const INITIAL_STATE = {
+  currencies: [],
+  expenses: [],
+};
+
+function walletReducer(state = INITIAL_STATE, action) {
+  console.log(action);
+  return state;
+}
+
+export default walletReducer;
