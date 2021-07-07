248118a1bafada8eb1564cc6db140f39e7d0e116
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2021-07-07 15:21:40 -0300

    Requisito 1 Realizado

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Route, Switch } from 'react-router';
+import LoginPage from './pages/Login';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <Switch>
+      <Route exact patch="/" component={ LoginPage } />
+    </Switch>
+  );
 }
 
 export default App;
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,15 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+// const INITIAL_STATE = {
+//   user: {
+//     email: '',
+//   },
+//   wallet: {
+//     currencies: [],
+//     expenses: [],
+//   },
+// };
+
+// function walletReducer(state = INITIAL_STATE, action) {
+//   switch (action.type) {
+//   }
+// }
