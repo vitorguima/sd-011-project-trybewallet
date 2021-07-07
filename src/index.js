ddbe07e24123caab1616427dbef45f66c8e3cc19
Renan <renan.vamo@gmail.com>
2021-07-07 14:42:06 -0300

    add o provider sem a store

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Provider store={  }>
+      <App />
+    </Provider>
   </BrowserRouter>,
   document.getElementById('root'),
 );
