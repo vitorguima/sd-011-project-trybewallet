78b259e913b0485c171f5cd9bb9fec39e94e05b7
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2021-07-07 14:42:54 -0300

    Iniciando project-trybewallet

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+// import Carteira from './pages/Carteira';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <div>
+      Hello, TrybeWallet!
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          {/* <Route path="/carteira" component={ Carteira } /> */}
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
