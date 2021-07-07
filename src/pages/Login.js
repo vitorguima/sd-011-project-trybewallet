e40559fedb8fc8388762523621329f894a46223a
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-07-07 17:19:54 -0300

    Adiciona validacao email e senha

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,26 +1,67 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+      enabledBtn: true,
+    };
+
+    this.handleUserInput = this.handleUserInput.bind(this);
+  }
+
+  checkUserData() {
+    const { email, password } = this.state;
+    const minPassword = 6;
+
+    if (email.includes('@') && email.includes('.com') && password.length >= minPassword) {
+      this.setState({
+        enabledBtn: false,
+      });
+    }
+  }
+
+  handleUserInput({ target }) {
+    const { name, value } = target;
+
+    this.setState({
+      [name]: value,
+    },
+    () => this.checkUserData());
+  }
+
   render() {
+    const { enabledBtn } = this.state;
+
     return (
       <div>
         <form>
           <fieldset>
-            <label htmlFor="email-input">
-              <input
-                type="email"
-                id="email-input"
-                data-testid="email-input"
-              />
-            </label>
-            <label htmlFor="password-input">
-              <input
-                type="password"
-                id="password-input"
-                data-testid="password-input"
-              />
-            </label>
-            <button type="button">Entrar</button>
+            <input
+              type="email"
+              id="email-input"
+              name="email"
+              data-testid="email-input"
+              placeholder="Digite seu e-mail"
+              onChange={ this.handleUserInput }
+            />
+            <input
+              type="password"
+              id="password-input"
+              name="password"
+              data-testid="password-input"
+              placeholder="Digite sua senha"
+              onChange={ this.handleUserInput }
+            />
+            <button
+              type="button"
+              disabled={ enabledBtn }
+            >
+              Entrar
+            </button>
           </fieldset>
         </form>
       </div>
