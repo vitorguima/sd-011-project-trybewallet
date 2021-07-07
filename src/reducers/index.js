55971cfc2bd978664b46bb579da27616b144d9a1
Tulio Sploradori <tuliospl@gmail.com>
2021-07-07 14:34:30 -0300

    iniciando o Pr

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,6 @@
 // import user from './user';
 // import wallet from './wallet';
+// BLZ
 
 // Configure os seus reducers.
 // ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
