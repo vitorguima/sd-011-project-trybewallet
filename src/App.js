3dbe079bc406e0bb929f505b8208a15677ef0a1a
Cristian Brum Oliveira <cristian.brum2@yahoo.com.br>
2021-07-07 20:30:29 -0300

    requisitos 1 ao 5

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Wallet from './pages/Wallet';
 
 class App extends React.Component {
   render() {
     return (
-      <Router>
+      <div className="App">
+
         <Switch>
           <Route exact path="/" component={ Login } />
           <Route path="/carteira" component={ Wallet } />
         </Switch>
-      </Router>
+
+      </div>
     );
   }
 }
