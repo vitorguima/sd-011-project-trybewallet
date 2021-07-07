8d03fab6090bfb57a507acd5c1f1c868a2a87222
KaioHPs <kaiohpsalternativo@gmail.com>
2021-07-07 18:00:19 -0300

    Requisitos 1, 2 e 3

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,6 @@
 // Coloque aqui suas actions
+export const newUser = (state) => ({ type: 'NEW_USER', state });
+
+export default {
+  newUser,
+};
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,82 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { newUser } from '../actions/index';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      pass: '',
+    };
+
+    this.changeState = this.changeState.bind(this);
+    this.canSubmit = this.canSubmit.bind(this);
+    this.handleButton = this.handleButton.bind(this);
+  }
+
+  changeState({ target }) {
+    const { name, value } = target;
+    this.setState(() => ({ [name]: value }));
+  }
+
+  canSubmit() {
+    const { email, pass } = this.state;
+    const filter = /^([a-zA-Z0-9_.-])+@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    const minPassLen = 6;
+    return ((email.length > 0 && filter.test(email)) && pass.length >= minPassLen);
+  }
+
+  handleButton(event) {
+    event.preventDefault();
+    const { emailDispatch, history } = this.props;
+    const { email } = this.state;
+
+    emailDispatch(email);
+    history.push('/carteira');
+  }
+
   render() {
-    return <div>Login</div>;
+    const isEnabled = this.canSubmit();
+
+    return (
+      <div>
+        <form id="login-cont">
+          <input
+            type="email"
+            name="email"
+            data-testid="email-input"
+            onChange={ this.changeState }
+          />
+          <input
+            type="password"
+            name="pass"
+            data-testid="password-input"
+            onChange={ this.changeState }
+          />
+          <button
+            type="submit"
+            disabled={ !isEnabled }
+            onClick={ this.handleButton }
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  emailDispatch: (state) => dispatch(newUser(state)) });
+
+Login.propTypes = {
+  emailDispatch: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
