29ab49bdd2d1d59ecaf2ea8c1ebbc30dded1bd00
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-07-07 15:00:12 -0300

    Adiciona estrutura basica pagina login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,29 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <form>
+          <fieldset>
+            <label htmlFor="email-input">
+              <input
+                type="email"
+                id="email-input"
+                data-testid="email-input"
+              />
+            </label>
+            <label htmlFor="password-input">
+              <input
+                type="password"
+                id="password-input"
+                data-testid="password-input"
+              />
+            </label>
+            <button type="button">Entrar</button>
+          </fieldset>
+        </form>
+      </div>
+    );
   }
 }
 
