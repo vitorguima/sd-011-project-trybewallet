33122d619e80c8fe36b9f727428d4bbf021e5506
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-07-07 17:39:25 -0300

    Adiciona funcionalidade de login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 function App() {
   return (
     <div>
       <Switch>
+        <Route path="/carteira" render={ () => <Wallet /> } />
         <Route path="/" render={ () => <Login /> } />
       </Switch>
     </div>
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,10 +1,15 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+import { LOGIN_USER } from '../actions';
+
 const INITIAL_STATE = {
   email: '',
+  isLogged: false,
 };
 
 const user = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+  case LOGIN_USER:
+    return { ...state, email: action.email, isLogged: true };
   default:
     return state;
   }
