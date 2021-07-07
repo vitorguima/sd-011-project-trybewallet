be0b8d41a7b88b66ebb52ca005dd45c50e3430ad
Víctor Faria <vitilevi@hotmail.com>
2021-07-07 17:52:27 -0300

    Corrigido login

---
+++ src/components/expensesForm.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class expensesForm extends Component {
+  render() {
+    return (
+      <form>
+        0
+      </form>
+    );
+  }
+}
