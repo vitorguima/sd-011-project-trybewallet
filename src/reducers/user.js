a6aa3a24631134a7b036800ef5fd592a8b45af80
Luiz Vicentin <luizpavanello@gmail.com>
2021-07-07 18:47:46 -0300

    Added the file Reducer User Initial State & Testing the Button "Entrar"

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,15 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const INICIAL_STATE = {
+  email: '',
+};
+
+export default function userReducer(state = INICIAL_STATE, action) {
+  switch (action.type) {
+  case 'SET_EMAIL':
+    return {
+      email: action.payload.email,
+    };
+  default:
+    return state;
+  }
+}
