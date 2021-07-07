d1c211e73ba58c4685042722bcbcadcf71b4749e
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2021-07-07 17:30:51 -0300

    adicionando funcao mapstate e connect para exportar o componente

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 class Wallet extends React.Component {
   render() {
+    const { email } = this.props;
     return (
       <header>
         <h1>TrybeWallet</h1>
-        <h4 data-testid="email-field">{`Bem vindo: ${ email }`}</h4>
+        <h4 data-testid="email-field">{`Bem vindo: ${email}`}</h4>
         <h4 data-testid="total-field">0</h4>
         <h4 data-testid="header-currency-field">BRL</h4>
       </header>
@@ -13,4 +15,8 @@ class Wallet extends React.Component {
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+export default connect(mapStateToProps)(Wallet);
