573fb3d8f1444599d253a2b8ed6f44a5ee835213
Marco Galindo <marco1408@gmail.com>
2021-07-07 15:44:38 -0300

    requisitos 1 e 2 finalizados

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,83 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { actionLogin } from '../actions';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      disabled: true,
+    };
+    this.handleInput = this.handleInput.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleInput({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, () => {
+      const { email, password } = this.state;
+      const passwordLength = 6;
+      const emailValid = email.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+      const passwordValid = password.length >= passwordLength;
+      this.setState({
+        disabled: !(emailValid && passwordValid),
+      });
+    });
+  }
+
+  handleClick() {
+    const { email } = this.state;
+    const { setLogin } = this.props;
+    setLogin(email);
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password, disabled } = this.state;
+
+    return (
+      <form>
+        <input
+          type="email"
+          name="email"
+          value={ email }
+          onChange={ this.handleInput }
+          data-testid="email-input"
+          placeholder="email"
+        />
+        <input
+          type="password"
+          name="password"
+          value={ password }
+          onChange={ this.handleInput }
+          data-testid="password-input"
+          placeholder="password"
+        />
+        <Link to="/carteira">
+          <button
+            type="button"
+            onClick={ this.handleClick }
+            disabled={ disabled }
+          >
+            Entrar
+          </button>
+        </Link>
+      </form>
+    );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  setLogin: (login) => dispatch(actionLogin(login)),
+});
+
+Login.propTypes = {
+  setLogin: PropTypes.func,
+}.isRequired;
+
+export default connect(null, mapDispatchToProps)(Login);
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,23 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+import { LOGIN } from '../actions';
+
+const INITIAL_STATE = {
+  user: {
+    email: '',
+  },
+  wallet: {
+    currencies: [],
+    expenses: [],
+  },
+};
+
+const user = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case LOGIN:
+    return { ...state, user: { email: action.payload } };
+  default:
+    return state;
+  }
+};
+
+export default user;
