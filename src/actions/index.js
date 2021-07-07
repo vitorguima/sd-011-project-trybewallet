e3e6ed4316bb0be9104f5841226f1f3ab23e9643
Víctor Faria <vitilevi@hotmail.com>
2021-07-07 16:50:22 -0300

    Feito requisitos 1, 2 e 3

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,12 @@
-// Coloque aqui suas actions
+export const SAVE_EMAIL = 'SAVE_EMAIL';
+export const AUTH_TO_LOGIN = 'AUTH_TO_LOGIN';
+
+export const saveEmail = (payload) => ({
+  type: SAVE_EMAIL,
+  payload,
+});
+
+export const authToLogin = (payload) => ({
+  type: AUTH_TO_LOGIN,
+  payload,
+});
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Route } from 'react-router';
 import Login from '../pages/Login';
+import Wallet from '../pages/Wallet';
 
 function Routes() {
   return (
     <>
       <Route exact path="/" component={ Login } />
-      <Route path="" />
+      <Route path="/carteira" component={ Wallet } />
     </>
   );
 }
