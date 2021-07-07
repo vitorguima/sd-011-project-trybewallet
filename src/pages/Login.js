f0f6814444b9a39d0a250410a76d9503fde32cd4
Marcelo <marsleite@gmail.com>
2021-07-07 15:43:05 -0300

    correção lint

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-export default function Login() {
+function Login() {
   return (
-    <div>
+    <>
       <input type="email" data-testid="email-input" />
       <input type="password" data-testid="password-input" />
       <button type="button">Entrar</button>
-    </div>
+    </>
   );
 }
+
+export default Login;
