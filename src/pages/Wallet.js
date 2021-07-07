34fe89fc5f946991469cfea24a3cbf4c4d3b751e
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2021-07-07 17:54:09 -0300

    criando todas as labels e inputs do forms

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -14,9 +14,40 @@ class Wallet extends React.Component {
           <h4 data-testid="header-currency-field">BRL</h4>
         </header>
 
-        <section class="form">
+        <section className="form">
           <form>
-            
+            <label htmlFor="value">
+              Valor
+              <input type="text" name="value" />
+            </label>
+            <label htmlFor="description">
+              Descrição
+              <input type="text" name="description" />
+            </label>
+            <label htmlFor="coin">
+              Moeda
+              <select name="coin">
+                <option></option>
+              </select>
+            </label>
+            <label htmlFor="payment-method">
+              Método de pagamento
+              <select name="payment-method">
+                <option value="money">Dinheiro</option>
+                <option value="credit-card">Cartão de crédito</option>
+                <option value="credit-card">Cartão de débito</option>
+              </select>
+            </label>
+            <label htmlFor="tag">
+              Tag
+              <select name="tag">
+                <option value="food">Alimentação</option>
+                <option value="leisure">Lazer</option>
+                <option value="work">Trabalho</option>
+                <option value="transport">Transporte</option>
+                <option value="health">Saúde</option>
+              </select>
+            </label>
           </form>
         </section>
       </main>
