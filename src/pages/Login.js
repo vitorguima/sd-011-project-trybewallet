ffaf3588ced2b14b186f9a8cd018834e004fcc0d
LeoFuna <leo.funa@gmail.com>
2021-07-07 15:25:45 -0300

    Requisito 2 iniciado com erro em interacao validacao botao

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,65 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      inputEmail: '',
+      inputPassword: '',
+    };
+    this.handleEvent = this.handleEvent.bind(this);
+    this.controlButton = this.controlButton.bind(this);
+  }
+
+  handleEvent(event) {
+    const { target: { value, name } } = event;
+    this.setState({
+      [name]: value,
+    });
+    this.controlButton();
+  }
+
+  verifyValidEmail(email) {
+    const pattern = /\S+@\S+\.\S+/;
+    return pattern.test(email);
+  }
+
+  verifyValidPassword(password) {
+    const minLength = 6;
+    return (password.length >= minLength);
+  }
+
+  controlButton() {
+    const { inputEmail, inputPassword } = this.state;
+    const loginButton = document.querySelectorAll('.myButton')[0];
+    if (this.verifyValidEmail(inputEmail) && this.verifyValidPassword(inputPassword)) {
+      loginButton.disabled = false;
+    } else {
+      loginButton.disabled = true;
+    }
+  }
+
   render() {
+    const { inputEmail, inputPassword } = this.state;
     return (
       <div>
-        <input data-testid="email-input" type="text" placeholder="Email" />
-        <input data-testid="password-input" type="password" placeholder="Senha" />
-        <input type="button" value="Entrar" />
+        <input
+          data-testid="email-input"
+          name="inputEmail"
+          onChange={ this.handleEvent }
+          value={ inputEmail }
+          type="text"
+          placeholder="Email"
+        />
+        <input
+          data-testid="password-input"
+          name="inputPassword"
+          onChange={ this.handleEvent }
+          value={ inputPassword }
+          type="password"
+          placeholder="Senha"
+        />
+        <input type="button" className="myButton" disabled value="Entrar" />
       </div>
     );
   }
