a783747080d962dcad2ab5ac9bcbe7136b4366dd
Diogo Augusto <diogotrescastro@gmail.com>
2021-07-07 16:01:52 -0300

    Resoluçao do requisito 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={ () => (
+              <Login />) }
+          />
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
