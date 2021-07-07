35e4707f1736029ff6d7ee79038d8986ebf4dbee
Arthur Hermann <arthurhermannc@gmail.com>
2021-07-07 15:13:33 -0300

    add actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,21 @@
 // Coloque aqui suas actions
+export const loginAction = (email) => ({ type: 'LOGIN', email });
+
+export const requestAPIAction = () => ({ type: 'REQUEST_API' });
+
+export const getCurrenciesAction = (data) => ({ type: 'GET_CURRENCIES', data });
+
+export const addExpenseAction = (expense) => ({ type: 'ADD_EXPENSE', expense });
+
+export const setExpenseAction = (expense) => async (dispatch) => {
+  const reponse = await fetch('https://economia.awesomeapi.com.br/json/all');
+  const json = await reponse.json(reponse);
+  dispatch(addExpenseAction({ ...expense, exchangeRates: json }));
+};
+
+export const fetchAPIAction = () => async (dispatch) => {
+  dispatch(requestAPIAction());
+  const reponse = await fetch('https://economia.awesomeapi.com.br/json/all');
+  const json = await reponse.json(reponse);
+  dispatch(getCurrenciesAction(json));
+};
