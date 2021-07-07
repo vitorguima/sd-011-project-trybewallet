f026023ed0e23a789f7a4f62d6636603a036ae65
Felipe Buso <felipe_buso@hotmail.com>
2021-07-07 15:47:56 -0300

    item 1

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,14 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_STATE = {
+  user: {
+    email: '',
+  },
+};
+
+function userReducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  default:
+    return state;
+  }
+}
+
+export default userReducer;
