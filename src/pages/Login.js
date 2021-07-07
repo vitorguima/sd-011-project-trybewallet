5974ada7b9c22d1ba6551e81f76cfd5a4fa79ccf
Guilherme Pereira de Oliveira <guilhermepdo1508@gmail.com>
2021-07-07 17:20:23 -0300

    Requisito 2 refatorado

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -9,44 +9,29 @@ class Login extends React.Component {
     super(props);
     this.state = {
       email: '',
-      password: '',
-      disableSubmit: true,
     };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleClick = this.handleClick.bind(this);
     this.enableButton = this.enableButton.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   enableButton() {
-    const { email, password } = this.state;
-    const validateEmail = (
-      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-    );
-    const isEmailValid = validateEmail.test(email);
-    const passwordMinSize = 5;
-    const isPasswordValid = password.length >= passwordMinSize;
-    const disableSubmit = (!isEmailValid || !isPasswordValid);
-    this.setState({ disableSubmit });
+    const form = document.querySelector('.login-form');
+    const btnSubmit = document.querySelector('.submit-button');
+    btnSubmit.disabled = !form.checkValidity();
   }
 
   handleChange({ target: { value, name } }) {
     this.setState({
       [name]: value,
     });
-    this.enableButton();
-  }
-
-  handleClick() {
-    const { email } = this.state;
-    const { emailDispatch } = this.props;
-    emailDispatch(email);
   }
 
   render() {
-    const { disableSubmit } = this.state;
+    const { email } = this.state;
+    const { emailDispatch } = this.props;
     return (
       <div className="login-page">
-        <div className="login-area">
+        <form className="login-form" onChange={ this.enableButton }>
           <input
             placeholder="e-mail"
             name="email"
@@ -57,22 +42,23 @@ class Login extends React.Component {
           />
           <input
             placeholder="senha"
-            name="password"
             type="password"
             data-testid="password-input"
+            pattern=".{6,}"
+            title="A senha deve ter no mínimo 6 caracteres"
             required
-            onChange={ this.handleChange }
           />
           <Link to="/carteira">
             <button
-              type="button"
-              onClick={ this.handleClick }
-              disabled={ disableSubmit }
+              className="submit-button"
+              type="submit"
+              onClick={ () => emailDispatch(email) }
+              disabled
             >
               Entrar
             </button>
           </Link>
-        </div>
+        </form>
       </div>
     );
   }
