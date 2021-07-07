d6d86e261247dc8d834bc34edfe62c7739fe9cae
Rodolfo <rodolforrcont@gmail.com>
2021-07-07 18:13:44 -0300

    Requisito 6 - Form de Despesas e Pagamentos

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -21,6 +21,40 @@ class Wallet extends React.Component {
           <p><span data-testid="total-field">{ spent }</span></p>
           <p><span data-testid="header-currency-field">BRL</span></p>
         </header>
+        <form>
+          <label htmlFor="valor">
+            Valor
+            <input type="text" name="valor" id="valor" />
+          </label>
+          <label htmlFor="descrição">
+            Descrição
+            <input type="text" name="descrição" id="descrição" />
+          </label>
+          <label htmlFor="coin">
+            Moeda
+            <select name="coin" id="coin">{}</select>
+          </label>
+          <label htmlFor="pagamento">
+            Método de pagamento
+            <select name="pagamento" id="pagamento">
+              <option>Selecione método de pagamento</option>
+              <option value="money">Dinheiro</option>
+              <option value="credit">Cartão de Crédito</option>
+              <option value="debit">Cartão de Débito</option>
+            </select>
+          </label>
+          <label htmlFor="tag">
+            Tag
+            <select name="tag" id="tag">
+              <option>Selecione categoria de despesa</option>
+              <option value="food">Alimentação</option>
+              <option value="leisure">Lazer</option>
+              <option value="work">Trabalho</option>
+              <option value="transport">Transporte</option>
+              <option value="health">Saúde</option>
+            </select>
+          </label>
+        </form>
       </div>
     );
   }
