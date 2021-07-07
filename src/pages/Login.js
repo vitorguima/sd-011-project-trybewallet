cf2063dbd45460e238fa3f4c44cc0bfb2a90d1a9
Gabriel <gabrielfernandessilva.ba@gmail.com>
2021-07-07 14:57:08 -0300

    Primeiro requisito

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,27 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.validateEmail = this.validateEmail.bind(this);
+  }
+
+  validateEmail({ target }) {
+    const reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+    if (reg.test(target.value)) {
+      return true;
+    }
+    return false;
+  }
+
   render() {
     return (
       <form>
-        <input type="email" data-testid="email-input" />
+        <input
+          onChange={ () => this.validateEmail() }
+          type="email"
+          data-testid="email-input"
+        />
         <input type="password" data-testid="password-input" />
         <button type="button">Entrar</button>
       </form>
