b492b4c542cebd1cd4a73be4aed87a597c9f2085
LeoFuna <leo.funa@gmail.com>
2021-07-07 14:00:04 -0300

    Iniciando Requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <input data-testid="email-input" type="text" placeholder="Email" />
+        <input data-testid="password-input" type="password" placeholder="Senha" />
+        <input type="button" value="Entrar" />
+      </div>
+    );
   }
 }
 
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,14 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+
+const INITIAL_STATE = {
+
+};
+
+function wallet(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  default:
+    return state;
+  }
+}
+
+export default wallet;
