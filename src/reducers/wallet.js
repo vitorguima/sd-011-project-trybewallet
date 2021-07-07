f697c0de2181412e43b1915396877543aff1cb41
Julio Filizzola <juliofilizzola@hotmail.com>
2021-07-07 20:58:16 -0300

    add isloadding

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,10 +1,12 @@
 const INITIAL_STATE = {
   currencies: [],
   expenses: [],
+  isLoading: false,
 };
 
 function wallet(state = INITIAL_STATE, action) {
   switch (action.type) {
+
   default:
     return state;
   }
