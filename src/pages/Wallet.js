deedaaab22716f67a175f1450835ba43ad592a75
Rodolfo <rodolforrcont@gmail.com>
2021-07-07 17:32:50 -0300

    Requisito 5 - Criado o Header, com e-mail do usuário, total de despesa começando com 0 e moeda de conversão

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Wallet extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      spent: 0,
+    };
+  }
+
   render() {
-    return <div>TrybeWallet</div>;
+    const { spent } = this.state;
+    const { email } = this.props;
+    return (
+      <div>
+        <div>TrybeWallet</div>
+        <header>
+          <p><span data-testid="email-field">{ email }</span></p>
+          <p><span data-testid="total-field">{ spent }</span></p>
+          <p><span data-testid="header-currency-field">BRL</span></p>
+        </header>
+      </div>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+export default connect(mapStateToProps)(Wallet);
+
+Wallet.propTypes = {
+  email: PropTypes.string,
+}.isRequired;
