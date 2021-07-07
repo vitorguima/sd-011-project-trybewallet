d56907192c0341f84b623e5f40344a0fbbe05955
gabiazevedo <gabiazevedoms@gmail.com>
2021-07-07 17:03:18 -0300

    Adiciona requisito 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Wallet from './pages/Wallet';
+import store from './store';
 
-function App() {
-  return <div>Hello, TrybeWallet!</div>;
+class App extends React.Component {
+  render() {
+    return (
+      <Provider store={ store }>
+        <BrowserRouter>
+          <div>Hello, TrybeWallet!</div>
+          <Route path="/" component={ Login } />
+          <Route path="/wallet" component={ Wallet } />
+        </BrowserRouter>
+      </Provider>
+    );
+  }
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <hi>Login</hi>
+        <label htmlFor="email">
+          E-mail
+          <input
+            id="email"
+            type="text"
+            data-testid="email-input"
+            name="email"
+            required
+          />
+        </label>
+        <label htmlFor="password">
+          Senha
+          <input
+            id="password"
+            type="text"
+            data-testid="password-input"
+            maxLength="6"
+            name="password"
+            required
+          />
+        </label>
+        <Link to="/wallet">
+          <button type="button">Entrar</button>
+        </Link>
+      </div>
+    );
   }
 }
 
