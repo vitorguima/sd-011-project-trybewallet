93492f2fd94ffe11cadc9464eaec602723e3bc07
Luiz Otavio <luiz.otavio273@gmail.com>
2021-07-07 18:49:31 -0300

    initial redux config

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,14 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_STATE = {
+  email: '',
+};
+
+const User = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case 'USER_EMAIL':
+    return state;
+  default:
+    return state;
+  }
+};
+
+export default User;
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,15 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+const INITIAL_STATE = {
+  currencies: [],
+  expenses: [],
+};
+
+const Wallet = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case 'CURRENCIES_EXPENSES':
+    return state;
+  default:
+    return state;
+  }
+};
+
+export default Wallet;
