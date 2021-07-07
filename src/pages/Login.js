32de3e400ad938e385b6b7268fb02bd3d9fa9a45
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-07-07 16:26:48 -0300

    push para teste do diegao

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,17 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <label>
+          <input data-testid="email-input" type="text"></input>
+        </label>
+        <label>
+          <input data-testid="password-input" type="password"></input>
+        </label>
+        <button></button>
+      </div>
+    );
   }
 }
 
