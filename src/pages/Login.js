6422166095c968012d8d72402fd8f66dddc86f5f
Rodolfo <rodolforrcont@gmail.com>
2021-07-07 14:49:28 -0300

    Requisito 1 - Input E-mail e Senha

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,26 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <div>Login</div>
+        <div>
+          <label htmlFor="email">
+            <input data-testid="email-input" type="email" placeholder="Digite o e-mail" />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="password">
+            <input
+              data-testid="password-input"
+              type="password"
+              placeholder="Digite a senha"
+            />
+          </label>
+        </div>
+        <button type="button">Entrar</button>
+      </div>
+    );
   }
 }
 
