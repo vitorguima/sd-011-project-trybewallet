d8d9482798076ee990bd16f8eae15c2f9a1705ab
guinhoanjos <iago.anjos@hotmail.com>
2021-07-07 15:24:11 -0300

    tela de login criado

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <>
+      <h1>TrybeWallet</h1>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+        </Switch>
+      </Router>
+    </>
+  );
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <>
+        <input type="text" data-testid="email-input" placeholder="Email" />
+        <input type="password" data-testid="password-input" placeholder="Senha" />
+        <button type="submit">Entrar</button>
+      </>
+    );
   }
 }
 
