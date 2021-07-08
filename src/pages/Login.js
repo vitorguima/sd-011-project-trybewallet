8f45f1c2afb2e1fdaf53d4b2801dab91de720a72
fernando lasmar <f3rnandolasmar@gmail.com>
2021-07-08 02:15:49 -0300

    requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,41 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      disable: true,
+    };
+    this.handleInputLogin = this.handleInputLogin.bind(this);
+    this.loginValidation = this.loginValidation.bind(this);
+  }
+
+  componentDidUpdate() {
+    this.loginValidation();
+  }
+
+  loginValidation() {
+    const { email, password, disable } = this.state;
+    const characters = 6;
+    if (email.split('').includes('@')
+      && email.split('.').includes('com')
+      && password.length >= characters
+      && disable) {
+      this.setState({ disable: false });
+    }
+  }
+
+  handleInputLogin({ target }) {
+    const { value, name } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const { email, password, disable } = this.state;
     return (
       <section>
 
@@ -14,7 +48,9 @@ class Login extends React.Component {
               id="email"
               name="email"
               data-testid="email-input"
-              placeholder="Email"
+              placeholder="Username"
+              value={ email }
+              onChange={ this.handleInputLogin }
             />
           </label>
         </div>
@@ -26,12 +62,14 @@ class Login extends React.Component {
               id="password"
               name="password"
               data-testid="password-input"
-              placeholder="Senha"
+              placeholder="Password"
+              value={ password }
+              onChange={ this.handleInputLogin }
             />
           </label>
         </div>
 
-        <button type="button">Entrar</button>
+        <button type="button" disabled={ disable }>Entrar</button>
 
       </section>
     );
