929fd59ec8dd95236f5aa8025049050e0e96a969
Alexandre <naitwa.alexandre@gmail.com>
2021-07-08 17:02:24 -0300

    03 lint

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,25 +1,19 @@
 import React, { useState } from 'react';
-import PropTypes from 'prop-types';
 import { useHistory } from 'react-router';
-import { connect } from 'react-redux';
-// import { useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { setForm } from '../actions';
 import Form from './Form';
 
-function Login({ user }) {
-  // const dispatch = useDispatch();
+function Login() {
+  const dispatch = useDispatch();
   const history = useHistory();
-  // const [isValidated, setIsValidated] = useState(true);
   const [inputForm, setInputForm] = useState({
     email: '',
     password: '',
   });
-  // const VALID_PASS = 6;
-  // const VALID_EMAIL = /^([a-zA-Z0-9_.-])+@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
   const handleSubmit = (e) => {
     e.preventDefault();
-    // dispatch(setForm(inputForm.email));
-    user(inputForm.email);
+    dispatch(setForm(inputForm.email));
     setInputForm({ email: '', password: '' });
     history.push({
       pathname: '/carteira',
@@ -38,17 +32,9 @@ function Login({ user }) {
     <Form
       handleChange={ handleChange }
       handleSubmit={ handleSubmit }
-      // isValidated={ isValidated }
       inputForm={ inputForm }
     />
   );
 }
-const mapDispatchToProps = (dispatch) => ({
-  user: (payload) => dispatch(setForm(payload)),
-});
 
-Login.propTypes = {
-  user: PropTypes.func.isRequired,
-};
-
-export default connect(null, mapDispatchToProps)(Login);
+export default Login;
