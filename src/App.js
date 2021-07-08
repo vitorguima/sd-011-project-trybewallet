5c3f37d59c632f40d68bafc31ff2e48dfba79865
Ivaldo Souza <souza1976ivaldo@gmail.com>
2021-07-08 08:30:34 -0300

    requisito 1 e 2 corrigidos 3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 function App() {
   return (
     <div>
-      Hello, TrybeWallet!
       <Switch>
+        <Route path="/carteira" component={ Wallet } />
         <Route path="/" component={ Login } />
       </Switch>
     </div>
