048f508b643f55c9600f0a5ad516275660a3eb24
Diogo Augusto <diogotrescastro@gmail.com>
2021-07-08 15:17:35 -0300

    Atualizaçao do requisito 6

--- src/components/FormExpenses.js
+++ src/components/FormExpenses.js
@@ -3,44 +3,42 @@ import React, { Component } from 'react';
 export default class FormExpenses extends Component {
   render() {
     return (
-      <div>
-        <form>
-          <label htmlFor="valor">
-            Valor
-            <input type="text" name="valor" id="valor" />
-          </label>
-          <label htmlFor="descricao">
-            Descricao
-            <input type="text" name="descricão" id="descricao" />
-          </label>
-          <label htmlFor="moeda">
-            Moeda
-            <select id="moeda">
-              <option>
-                1
-              </option>
-            </select>
-          </label>
-          <label htmlFor="pagamento">
-            Método de pagamento
-            <select id="pagamento">
-              <option>Dinheiro</option>
-              <option>Cartão de crédito</option>
-              <option>Cartão de débito</option>
-            </select>
-          </label>
-          <label htmlFor="tag">
-            Tag
-            <select id="tag">
-              <option>Alimentação</option>
-              <option>Lazer</option>
-              <option>Trabalho</option>
-              <option>Transporte</option>
-              <option>Saúde</option>
-            </select>
-          </label>
-        </form>
-      </div>
+      <form>
+        <label htmlFor="valor">
+          Valor
+          <input type="text" name="valor" id="valor" />
+        </label>
+        <label htmlFor="descricao">
+          Descrição
+          <textarea name="descricao" id="descricao" />
+        </label>
+        <label htmlFor="moeda">
+          Moeda
+          <select id="moeda" name="moeda">
+            {/* <option>
+              1
+            </option> */}
+          </select>
+        </label>
+        <label htmlFor="pagamento">
+          Método de pagamento
+          <select id="pagamento" name="pagamento">
+            <option>Dinheiro</option>
+            <option>Cartão de crédito</option>
+            <option>Cartão de débito</option>
+          </select>
+        </label>
+        <label htmlFor="tag">
+          Tag
+          <select id="tag" name="tag">
+            <option>Alimentação</option>
+            <option>Lazer</option>
+            <option>Trabalho</option>
+            <option>Transporte</option>
+            <option>Saúde</option>
+          </select>
+        </label>
+      </form>
     );
   }
 }
