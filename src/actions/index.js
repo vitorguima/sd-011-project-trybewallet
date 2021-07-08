77747f32c4dc21fa1a2bbe15dbe478558b8cf725
Ghislaine <glenox79@gmail.com>
2021-07-08 12:22:36 -0300

    Add checking input and btn fields

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,6 @@
-// Coloque aqui suas actions
+export const USER_LOGIN = 'USER_LOGIN';
+
+export const userLoginAction = (email) => ({
+  type: USER_LOGIN,
+  email,
+});
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,30 +1,59 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { userLoginAction } from '../actions/index';
 
 class Login extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       email: '',
       password: '',
+      btnDisabled: true,
     };
+
+    this.handleChangeInput = this.handleChangeInput.bind(this);
+    this.checkLogin = this.checkLogin.bind(this);
+  }
+
+  componentDidUpdate() {
+    this.checkLogin();
+  }
+
+  checkLogin() {
+    const { email, password, btnDisabled } = this.state;
+    const regEmail = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+    const checkEmail = regEmail.test(email);
+    const numLength = 6;
+    const checkPassword = password.length >= numLength;
+    if (checkEmail && checkPassword && btnDisabled) {
+      this.setState({ btnDisabled: false });
+    } else if ((!checkEmail || !checkPassword) && !btnDisabled) {
+      this.setState({ btnDisabled: true });
+    }
   }
 
   handleChangeInput({ target }) {
+    const { name, value } = target;
     this.setState({
-      [target.name]: target.value,
+      [name]: value,
     });
   }
 
   render() {
-    const { email, password } = this.state;
+    const { email, password, btnDisabled } = this.state;
+    const { validateLogin } = this.props;
     return (
-      <form>
+      <div>
+        <h2>TrybeWallet</h2>
         <input
           data-testid="email-input"
           type="email"
           placeholder="Informe seu email"
           name="email"
           value={ email }
+          required
           onChange={ this.handleChangeInput }
         />
         <input
@@ -33,14 +62,29 @@ class Login extends React.Component {
           placeholder="Informe sua senha"
           name="password"
           value={ password }
+          required
           onChange={ this.handleChangeInput }
         />
-        <button type="button">
-          Entrar
-        </button>
-      </form>
+        <Link to="/carteira">
+          <button
+            type="button"
+            disabled={ btnDisabled }
+            onClick={ () => validateLogin(email, password) }
+          >
+            Entrar
+          </button>
+        </Link>
+      </div>
     );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  validateLogin: (email) => dispatch(userLoginAction(email)),
+});
+
+export default connect(null, mapDispatchToProps)(Login);
+
+Login.propTypes = {
+  validateLogin: PropTypes.func,
+}.isRequired;
