21164280ab636bc41fa0246d3ae6cb6d6bc808a3
Gustavo Miyagawa <miyagawasantos@hotmail.com>
2021-07-08 03:33:52 -0400

    Requisito 1, feito.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,26 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <>
+        Login
+        <input
+          type="text"
+          placeholder="e-mail"
+          data-testid="email-input"
+        />
+        <input
+          type="text"
+          placeholder="senha"
+          data-testid="password-input"
+        />
+        <button
+          type="button"
+        >
+          Entrar
+        </button>
+      </>
+    );
   }
 }
 
