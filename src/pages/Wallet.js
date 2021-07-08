5f94dc0c838d64c92831ed7c355c5e7c33cfc316
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-07-07 23:13:15 -0300

    Requisito 6

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,16 +1,60 @@
+/* eslint-disable max-lines-per-function */
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 class Wallet extends React.Component {
-  render () {
+  render() {
     const { email } = this.props;
     return (
-      <div>
-        <div className="title">TrybeWallet</div>
-        <p data-testid="email-field">{email}</p>
-        <p data-testid="total-field">0</p>
-        <p data-testid="header-currency-field">BRL</p>
+      <div
+        className="main-page"
+      >
+        <section>
+          <div className="title">TrybeWallet</div>
+          <p data-testid="email-field">{email}</p>
+          <p data-testid="total-field">0</p>
+          <p data-testid="header-currency-field">BRL</p>
+        </section>
+        <form>
+          <label htmlFor="expenses">
+            Valor
+            <input id="expenses" type="number" />
+          </label>
+          {/*  */}
+          <label htmlFor="description">
+            Descrição
+            <input id="description" />
+          </label>
+          {/*  */}
+          <label htmlFor="currency">
+            Moeda
+            <select id="currency">
+              <option>a</option>
+            </select>
+          </label>
+          {/*  */}
+          <label htmlFor="payment-method">
+            Método de pagamento
+            <select id="payment-method">
+              <option>Dinheiro</option>
+              <option>Cartão de crédito</option>
+              <option>Cartão de débito</option>
+            </select>
+          </label>
+          {/*  */}
+          <label htmlFor="category">
+            Tag
+            <select id="category">
+              <option>Alimentação</option>
+              <option>Lazer</option>
+              <option>Trabalho</option>
+              <option>Transporte</option>
+              <option>Saúde</option>
+            </select>
+          </label>
+          {/*  */}
+        </form>
       </div>
     );
   }
