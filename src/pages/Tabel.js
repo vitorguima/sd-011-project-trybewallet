3e5dc8264cb3d190f58e56ebf6525d833ecc755b
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2021-07-08 12:51:02 -0300

    criando componente tabela e passando a semantica para ela

---
+++ src/pages/Tabel.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class Table extends Component {
+  render() {
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    );
+  }
+}
+
+export default Table;
