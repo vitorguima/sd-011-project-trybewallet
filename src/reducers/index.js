3ed688317724f7adacedfb4df0deb04d2d089f22
Luiz Vicentin <luizpavanello@gmail.com>
2021-07-07 21:57:29 -0300

    3 → Use Redux to save the information of the logged in person in the global state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,11 @@
 import { combineReducers } from 'redux';
-import user from './user';
-import wallet from './wallet';
+import userReducer from './user';
+import walletReducer from './wallet';
 
 // Configure os seus reducers.
 // ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
 
 export default combineReducers({
-  user,
-  wallet,
+  user: userReducer,
+  wallet: walletReducer,
 });
