a6c6e6088a0c182ccffe59d19c8ddfcb29c6775a
Daniel <danielfdoficial@gmail.com>
2021-07-07 22:04:27 -0300

    arruma o lint

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,5 +1,20 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+// Mudar a função depois.
 const INITIAL_STATE = {
   currencies: [],
   expenses: [],
 };
+
+function saveWallet(state = INITIAL_STATE, { type, payload }) {
+  switch (type) {
+  case 'ADD_EMAIL':
+    return {
+      ...state,
+      email: payload,
+    };
+  default:
+    return state;
+  }
+}
+
+export default saveWallet;
