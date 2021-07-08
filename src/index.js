61d427e7f0d8606449dbceea9da9ee0e5b45c643
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-07-08 17:41:30 -0300

    Cria o store e define o Provider

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
+import store from './store';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <Provider store={ store }>
     <App />
-  </BrowserRouter>,
+  </Provider>,
   document.getElementById('root'),
 );
 
