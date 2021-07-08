6e95767754f2837fb0303a1496ca7d521ee1f366
Diogo Augusto <diogotrescastro@gmail.com>
2021-07-08 13:13:02 -0300

    Resoluçao do requisito 3

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Wallet from './pages/Wallet';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Switch>
-          <Route
-            exact
-            path="/"
-            render={ () => (
-              <Login />) }
-          />
-          <Route
-            exact
-            path="/carteira"
-            component={ Wallet }
-          />
-        </Switch>
-      </Router>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={ () => (
+            <Login />) }
+        />
+        <Route
+          exact
+          path="/carteira"
+          component={ Wallet }
+        />
+      </Switch>
     </div>
   );
 }
