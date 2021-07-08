da451c0ae22c50646e00e054222cc57482dcf4c3
Luan Xavier <luantenorio3@gmail.com>
2021-07-08 16:08:21 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import Routes from './routes';
+import './App.css';
 
 function App() {
-  return <div>Hello, TrybeWallet! Iniciando o projeto!</div>;
+  return (
+    <div className="App">
+      <Routes />
+    </div>
+  );
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,32 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div className="login-container">
+        <div className="login-card">
+          <h3>Login</h3>
+          <div className="input-container">
+            <label htmlFor="email-input">
+              <input
+                id="email-input"
+                data-testid="email-input"
+                type="text"
+                placeholder="Digite seu email:"
+              />
+            </label>
+            <label htmlFor="password-input">
+              <input
+                id="password-input"
+                data-testid="password-input"
+                type="text"
+                placeholder="Digite sua senha:"
+              />
+            </label>
+            <button type="button">Entrar</button>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
