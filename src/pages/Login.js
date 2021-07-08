0c83e00736dd3bb8601cc76ab45379571ae7269e
anagiorgiani <cavalheiro.anacarolina@gmail.com>
2021-07-07 22:41:10 -0300

    requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,52 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      senha: '',
+      habilitar: false,
+    };
+
+    this.validateEmail = this.validateEmail.bind(this);
+    this.validateSenha = this.validateSenha.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+
+    this.setState({
+      [name]: value,
+    });
+
+    const { email, senha } = this.state;
+
+    if (name === 'email' || name === 'senha') {
+      const emailVar = (name === 'email' ? value : email);
+      const senhaVar = (name === 'senha' ? value : senha);
+      this.setState({ habilitar: (this.validateEmail(emailVar)
+      && this.validateSenha(senhaVar)) });
+    }
+  }
+
+  validateEmail(email) {
+    const re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  }
+
+  validateSenha(senha) {
+    const tamanhoSenha = 5;
+    if (senha.length > tamanhoSenha) {
+      return true;
+    }
+    return false;
+  }
+
   render() {
+    const { email, senha, habilitar } = this.state;
     return (
       <div>
         <h1>Login</h1>
@@ -13,6 +58,8 @@ class Login extends React.Component {
             data-testid="email-input"
             name="email"
             type="text"
+            onChange={ this.handleChange }
+            value={ email }
             required
           />
         </label>
@@ -23,12 +70,19 @@ class Login extends React.Component {
             data-testid="password-input"
             name="senha"
             type="text"
-            maxLength="6"
+            minLength="6"
+            onChange={ this.handleChange }
+            value={ senha }
             required
           />
         </label>
         <Link to="/wallet">
-          <button type="button">Entrar</button>
+          <button
+            type="button"
+            disabled={ !habilitar }
+          >
+            Entrar
+          </button>
         </Link>
       </div>
     );
