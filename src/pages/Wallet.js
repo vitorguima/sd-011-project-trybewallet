61c2b09779081c4c97b9f1d82015c7d9cdb5b587
Tales Coelho <tales-coelho@live.com>
2021-07-07 23:43:35 -0300

    Requisito 8 Terminado Ajustando erro de lint

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { fetchPosts } from '../actions';
 
-export class Wallet extends Component {
+class Wallet extends Component {
   constructor(props) {
     super(props);
 
