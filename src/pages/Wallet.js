6375996bd79facb4c7a02dd7c2d69f444aefe9a5
MatheusAraujoDev <matheusaraujo.dev@gmail.com>
2021-07-08 18:28:28 -0300

    Adiciona requisito 06

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -12,6 +12,44 @@ class Wallet extends React.Component {
           <p data-testid="total-field">{ 0 }</p>
           <p data-testid="header-currency-field">BRL</p>
         </header>
+
+        <form>
+          <label htmlFor="expenses-input">
+            Valor:
+            <input id="expenses-input" />
+          </label>
+
+          <label htmlFor="expenses-description">
+            Descrição:
+            <textarea id="expenses-description" />
+          </label>
+
+          <label htmlFor="currency-input">
+            Moeda:
+            <select id="currency-input"> </select>
+          </label>
+
+          <label htmlFor="payment-method-input">
+            Método de pagamento:
+            <select id="payment-method-input">
+              <option>Dinheiro</option>
+              <option>Cartão de crédito</option>
+              <option>Cartão de débito</option>
+            </select>
+          </label>
+
+          <label htmlFor="tags-input">
+            tag:
+            <select id="tags-input">
+              <option>Alimentação</option>
+              <option>Lazer</option>
+              <option>Trabalho</option>
+              <option>Transporte</option>
+              <option>Saúde</option>
+            </select>
+          </label>
+
+        </form>
       </div>
     );
   }
