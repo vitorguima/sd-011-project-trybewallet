59291d2c484eed84ceb53989bf87924b8c4087ad
Julio Filizzola <juliofilizzola@hotmail.com>
2021-07-08 08:13:13 -0300

    add momoney como valor

--- src/components/LabelForm.js
+++ src/components/LabelForm.js
@@ -31,7 +31,7 @@ export default class LabelForm extends Component {
           Método de pagamento
           <select id="pay">
             <option> Metodo de pagamento! </option>
-            <option>Dinheiro </option>
+            <option valor="money">Dinheiro </option>
             <option>Cartão de crédito </option>
             <option>Cartão de débito </option>
           </select>
