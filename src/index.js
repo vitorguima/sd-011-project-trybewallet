c551f1b836a35598d7233b0f98d6b429e37d8653
Reginara Gomes <refagomes@gmail.com>
2021-07-08 15:37:01 -0300

    Importa store. Sera?

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
-// import store from './store/index';
 import * as serviceWorker from './serviceWorker';
+import store from './store';
 
 ReactDOM.render(
   <Provider store={ store }>
