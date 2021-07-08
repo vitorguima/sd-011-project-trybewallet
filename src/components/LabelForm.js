ead200af361b6efa76d07977d84c4089cba1397c
Julio Filizzola <juliofilizzola@hotmail.com>
2021-07-08 10:05:37 -0300

    refaturando tag name

--- src/components/LabelForm.js
+++ src/components/LabelForm.js
@@ -71,7 +71,7 @@ export default class LabelForm extends Component {
           Tag
           <select
             id="Tag"
-            name='tag'
+            name="tag"
             value={ tag }
             onChange={ this.handleChange }
           >
