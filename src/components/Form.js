8693e5aa1f738a2abed6f3a6db4b385dc8a54d5a
Leandro Lopes <leandro_carlos@uol.com.br>
2021-07-08 16:25:58 -0300

    item 6 corrigido-3

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,31 +6,35 @@ class Form extends React.Component {
       <form>
         <label htmlFor="value">
           Valor:
-          <input type="text" name="value" />
+          <input type="text" name="value" id="value" />
         </label>
+
         <label htmlFor="description">
           Descrição:
-          <input type="text" name="description" />
+          <input type="text" name="description" id="description" />
         </label>
+
         <label htmlFor="currency">
           Moeda:
-          <select type="text" name="currency">
+          <select type="text" name="currency" id="currency">
             <option>
               {}
             </option>
           </select>
         </label>
+
         <label htmlFor="payment">
           Método de pagamento:
-          <select type="text" name="payment">
+          <select type="text" name="payment" id="payment">
             <option>Dinheiro</option>
             <option>Cartão de crédito</option>
             <option>Cartão de débito</option>
           </select>
         </label>
+
         <label htmlFor="tag">
-          tag:
-          <select type="text" name="tag">
+          Tag:
+          <select type="text" name="tag" id="tag">
             <option>Alimentação</option>
             <option>Lazer</option>
             <option>Trabalho</option>
