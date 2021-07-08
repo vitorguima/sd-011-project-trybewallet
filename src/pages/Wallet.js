0f14610bc4d152c10804fd063bf9f30e1a1fa25b
guinhoanjos <iago.anjos@hotmail.com>
2021-07-08 15:47:28 -0300

    requisito 6 feito

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -6,13 +6,49 @@ class Wallet extends React.Component {
   render() {
     const { emailState } = this.props;
     return (
-      <header>
-        <h3 data-testid="email-field">
-          {emailState}
-        </h3>
-        <p data-testid="header-currency-field">BRL</p>
-        <p data-testid="total-field">0</p>
-      </header>
+      <>
+        <header>
+          <h3 data-testid="email-field">
+            {emailState}
+          </h3>
+          <p data-testid="header-currency-field">BRL</p>
+          <p data-testid="total-field">0</p>
+        </header>
+        <form>
+          <label htmlFor="input-valor">
+            Valor
+            <input type="number" name="input-valor" id="input-valor" />
+          </label>
+          <label htmlFor="input-descricao">
+            Descrição
+            <input type="number" name="input-descricao" id="input-descricao" />
+          </label>
+          <label htmlFor="input-select-coin">
+            Moeda
+            <select id="input-select-coin">
+              <option valor="xablau">xablau</option>
+            </select>
+          </label>
+          <label htmlFor="input-select-method">
+            Método de pagamento
+            <select id="input-select-method">
+              <option valor="monney">Dinheiro</option>
+              <option valor="credit">Cartão de crédito</option>
+              <option valor="debit">Cartão de débito</option>
+            </select>
+          </label>
+          <label htmlFor="input-select-category">
+            Categorias
+            <select id="input-select-category">
+              <option valor="alimentation">Alimentação</option>
+              <option valor="fun">Lazer</option>
+              <option valor="work">Trabalho</option>
+              <option valor="transport">Transporte</option>
+              <option valor="health">Saúde</option>
+            </select>
+          </label>
+        </form>
+      </>
     );
   }
 }
