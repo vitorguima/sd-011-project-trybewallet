f8b5372b5df5f8e2706799dc3f18c416dab396be
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-07-08 17:42:32 -0300

    Requisito 1 - cria pagina inicial de login

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Login from './pages/Login';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <BrowserRouter>
+      <div>Hello, TrybeWallet!</div>
+      <Route exact path="/" component={ Login } />
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        Login
+        <input data-testid="email-input" type="email" />
+        <input data-testid="password-input" type="password" />
+        <button type="submit">Entrar</button>
+      </div>
+    );
   }
 }
 
