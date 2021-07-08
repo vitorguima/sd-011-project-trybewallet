a80166509125e51d616efdfbf6d6457ad2ce1d9d
Felipe Buso <felipe_buso@hotmail.com>
2021-07-08 12:43:54 -0300

    Item 5

---
+++ src/components/Header.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+class Header extends Component {
+  render() {
+    const { userEmail } = this.props;
+    console.log(userEmail);
+    return (
+      <div>
+        <label htmlFor="email-field">
+          Email:
+          <span data-testid="email-field">{ userEmail }</span>
+        </label>
+        <label htmlFor="total-field">
+          Total de gastos: $
+          <span data-testid="total-field">{ 0 }</span>
+        </label>
+        <span data-testid="header-currency-field">BRL</span>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => (
+  { userEmail: state.user.email }
+);
+
+export default connect(mapStateToProps)(Header);
+
+Header.propTypes = {
+  userEmail: PropTypes.string.isRequired,
+};
