a4d5c2ceab114f36200417efc58480991ee0a6a9
Leandro Lopes <leandro_carlos@uol.com.br>
2021-07-08 10:23:56 -0300

    item 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import './login.css';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <header>Trybe Wallet</header>
+        <div className="div">
+          <form>
+            <label htmlFor="email-imput">
+              Email
+              <input type="text" data-testid="email-input" />
+            </label>
+
+            <label htmlFor="password-input">
+              Senha
+              <input type="text" data-testid="password-input" />
+            </label>
+
+            <button type="button"> Entrar </button>
+          </form>
+        </div>
+        <footer>Desenvolvido por Leandro Lopes</footer>
+      </div>
+    );
   }
 }
 
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,16 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+const GET_EXPENSES = {
+  currencies: [],
+  expenses: [],
+};
+
+const expenses = (state = GET_EXPENSES, action) => {
+  switch (action.type) {
+  case 'WALLET':
+    return { ...state };
+  default:
+    return state;
+  }
+};
+
+export default expenses;
