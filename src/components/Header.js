9a04c87538817818c70176ee143aa9fceb5ef44e
Luiz Vicentin <luizpavanello@gmail.com>
2021-07-08 09:49:02 -0300

    Create a header for the Wallet Page

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+class Header extends React.Component {
+  render() {
+    const { user } = this.props;
+    return (
+      <header>
+        <h1>Trybe Wallet</h1>
+        <h2 data-testid="email-field">{ user || 'name' }</h2>
+        <h2 data-testid="total-field">0</h2>
+        <h2 data-testid="header-currency-field">BRL</h2>
+      </header>
+    );
+  }
+}
+
+Header.propTypes = {
+  user: PropTypes.string,
+}.isRequired;
+
+const mapStateToProps = (state) => ({
+  user: state.user.email,
+});
+
+export default connect(mapStateToProps)(Header);
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,16 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+const INICIAL_STATE = {
+  currencies: [],
+  expenses: [],
+};
+
+export default function walletReducer(state = INICIAL_STATE, action) {
+  switch (action.type) {
+  case 'SET_EMAIL':
+    return {
+      email: action.payload.email,
+    };
+  default:
+    return state;
+  }
+}
