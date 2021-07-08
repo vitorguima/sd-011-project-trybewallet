9c39bbee7a305f4cfb7b08e2c3ef02de5c914371
Bruno Davi <brunodaviandrade2000@gmail.com>
2021-07-08 18:42:07 -0300

    Login Test

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,5 @@
-// Coloque aqui suas actions
+export const SET_USER = 'SET_USER';
+export const SET_WALLET = 'SET_WALLET';
+
+export const setUser = (email) => ({ type: SET_USER, email });
+export const setWallet = (payload) => ({ type: SET_WALLET, payload });
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,17 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import { SET_WALLET } from '../actions';
+
+const initialState = {
+  currencies: [],
+  expenses: [],
+};
+
+const wallet = (state = initialState, { type, payload }) => {
+  switch (type) {
+  case SET_WALLET:
+    return { ...state, ...payload };
+
+  default:
+    return state;
+  }
+};
+export default wallet;
