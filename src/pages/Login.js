c07d3a554574c9d948c4f131586615a20b6a95d2
Laura Gusmao <laurargus@gmail.com>
2021-07-08 17:49:54 -0300

    Login Page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,73 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { PropTypes } from 'prop-types';
+import { connect } from 'react-redux';
+import { loginAction } from '../actions';
+import './login.css';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+
+    this.handleInput = this.handleInput.bind(this);
+  }
+
+  handleInput(event) {
+    const { value, name } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    const { login } = this.props;
+    const emailFormat = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    // Ref: https://stackoverflow.com/questions/940577/javascript-regular-expression-email-validation
+    const passwordLength = 6;
+    return (
+      <form className="login-form">
+        <input
+          type="text"
+          data-testid="email-input"
+          name="email"
+          placeholder="E-mail"
+          onChange={ this.handleInput }
+          value={ email }
+        />
+        <input
+          type="password"
+          data-testid="password-input"
+          name="password"
+          placeholder="Senha"
+          onChange={ this.handleInput }
+          value={ password }
+        />
+        <Link to="/carteira">
+          <button
+            type="button"
+            disabled={ !emailFormat.test(email) || password.length < passwordLength }
+            onClick={ () => login(email, password) }
+          >
+            Entrar
+          </button>
+        </Link>
+      </form>
+    );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  login: (email, password) => dispatch(loginAction(email, password)),
+});
+
+Login.propTypes = {
+  login: PropTypes.func.isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,18 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+import { USER_LOGIN } from '../actions';
+
+const INITIAL_STATE = {
+  email: '',
+  password: '',
+};
+
+function userReducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case USER_LOGIN:
+    return { ...state, email: action.payload.email, password: action.payload.password };
+  default:
+    return state;
+  }
+}
+
+export default userReducer;
