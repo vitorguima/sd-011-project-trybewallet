e3faf486e18b96746c5c687556bdfe11483275bc
Julio Filizzola <juliofilizzola@hotmail.com>
2021-07-08 11:08:18 -0300

    expenses reduce

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -5,13 +5,6 @@ import { fetchApi } from '../actions';
 import LabelForm from '../components/LabelForm';
 
 class Wallet extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      valor: 0,
-    };
-  }
-
   componentDidMount() {
     const { getApi } = this.props;
     getApi();
@@ -19,7 +12,7 @@ class Wallet extends React.Component {
 
   render() {
     const { email, coins, expenses, getApi } = this.props;
-    const { valor } = this.state;
+    const valor = expenses.reduce((acc, { exchangeRates, currency, value }) => acc + (Number(exchangeRates[currency].ask * value)), 0);
     return (
       <>
         <header>
@@ -27,7 +20,7 @@ class Wallet extends React.Component {
           <div data-testid="email-field">{ email }</div>
           <div data-testid="total-field">
             {' '}
-            { valor }
+            { valor || '0' }
             {' '}
           </div>
           <div data-testid="header-currency-field"> BRL </div>
