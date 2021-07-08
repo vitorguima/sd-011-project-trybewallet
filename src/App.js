f16da23767d4a1214897e3487e20853e84eaff7e
Reginara Gomes <refagomes@gmail.com>
2021-07-07 21:03:44 -0300

    Adiciona requisito 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Route, Switch } from 'react-router';
+import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 function App() {
-  return <div>Hello, TrybeWallet!!!</div>;
+  return (
+    <main>
+      <div>Hello, TrybeWallet!!!</div>
+      <switch>
+        <Route exact path="/" component={ Login } />
+        <Route exact path="/carteira" component={ Wallet } />
+      </switch>
+    </main>
+  );
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <main>
+        <h1>Login</h1>
+        <input data-testid="email-input" placeholder="Digite aqui o seu e-mail" />
+        <input data-testid="password-input" placeholder="Senha" />
+        <button type="button" disabled>Entrar</button>
+      </main>
+    );
   }
 }
 
