58224c671ed0fba04e81247555bcd509c9a867c4
Luiz Vicentin <luizpavanello@gmail.com>
2021-07-07 22:04:03 -0300

    3 → Use Redux to save the information of the logged in person in the global state 1.1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React from 'react';
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
-import store from './store';
+// import store from './store';
 import Login from './pages/Login';
-// import Wallet from './pages/Wallet';
+import Wallet from './pages/Wallet';
 
 function App() {
   return (
-    <Provider store={ store }>
-      <Switch>
-        <Route exact path="/" component={ Login } />
-        {/* <Route exact path="/carteira" component={ Wallet } /> */}
-      </Switch>
-    </Provider>
+    <Switch>
+      <Route exact path="/" component={ Login } />
+      <Route exact path="/carteira" component={ Wallet } />
+    </Switch>
   );
 }
 
