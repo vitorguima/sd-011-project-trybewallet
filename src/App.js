c1f5f2143dd0495455921b0e554b4624fae7173d
tomsantos07 <xotmsantosx@live.com>
2021-07-08 10:31:15 -0300

    corrigindo arquivo app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import './App.css';
 
 function App() {
-  return (
-    <Router>
-      <div>Movie Card Library CRUD</div>
-    </Router>
-  );
+  return <div>Hello, TrybeWallet!</div>;
 }
 
 export default App;
