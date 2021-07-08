6da4b3f208026ee99300c747be763489bf1460b5
Hugo Mouto <hmoutol@gmail.com>
2021-07-07 23:28:50 -0300

    feat: Critério 6 - check

--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -15,6 +15,39 @@ const Wallet = () => {
         <div data-testid="header-currency-field">
           BRL
         </div>
+        <form>
+          <label>
+            Valor
+            <input type="number" name="name" />
+          </label>
+          <label>
+            Descrição
+            <input type="text" name="name" />
+          </label>
+          <label>
+            Moeda
+            <select name="" id="">
+            </select>
+          </label>
+          <label>
+            Método de pagamento
+            <select name="" id="">
+              <option value="Dinheiro">Dinheiro</option>
+              <option value="Cartão de crédito">Cartão de crédito</option>
+              <option value="Cartão de débito">Cartão de débito</option>
+            </select>
+          </label>
+          <label>
+            Tag
+            <select name="" id="">
+              <option value="Alimentação">Alimentação</option>
+              <option value="Lazer">Lazer</option>
+              <option value="Trabalho">Trabalho</option>
+              <option value="Transporte">Transporte</option>
+              <option value="Saúde">Saúde</option>
+            </select>
+          </label>
+        </form>
       </nav>
     </>
   )
