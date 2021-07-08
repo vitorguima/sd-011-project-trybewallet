8d02a113608973e25a30e67186aed05e32a97d3f
JoaoCabrera <joaocabrerati@gmail.com>
2021-07-08 17:48:41 -0300

    Adiciona requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,26 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <h2>Login</h2>
+        <input
+          type="email"
+          placeholder="Email"
+          data-testid="email-input"
+        />
+        <input
+          type="password"
+          placeholder="Senha"
+          data-testid="password-input"
+        />
+        <button
+          type="button"
+        >
+          Entrar
+        </button>
+      </div>
+    );
   }
 }
 
