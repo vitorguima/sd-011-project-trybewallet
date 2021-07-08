0bdf5b6300b14a7af427335c0a5b8e64ed7d8807
Matheus Gaspar <msouzagaspar@gmail.com>
2021-07-08 11:48:27 -0300

    requisito 4

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Wallet from "./pages/Wallet";
 function App() {
   return (
     <Switch>
-      <Route path="/" component={Login} />
+      <Route path="/" exact component={Login} />
       <Route path="/carteira" component={Wallet} />
     </Switch>
   );
