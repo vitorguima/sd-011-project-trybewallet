571ed1b21e54a8481ca83ffbe876aa083e33d201
Leandro Lopes <leandro_carlos@uol.com.br>
2021-07-08 11:47:46 -0300

    item 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,34 @@
 import React from 'react';
 import './login.css';
+import { Link } from 'react-router-dom';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      passaword: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  buttonLogin() {
+    const { email, passaword } = this.state;
+    const emailValid = /\S+@\S+\.\S+/;
+    const passawordCharacters = 6;
+    if (emailValid.test(email) === true && passaword.length >= passawordCharacters) {
+      return false;
+    }
+    return true;
+  }
+
   render() {
     return (
       <div>
@@ -10,15 +37,31 @@ class Login extends React.Component {
           <form>
             <label htmlFor="email-imput">
               Email
-              <input type="text" data-testid="email-input" />
+              <input
+                type="text"
+                name="email"
+                data-testid="email-input"
+                onChange={ this.handleChange }
+                placeholder="E-mail"
+              />
             </label>
 
             <label htmlFor="password-input">
               Senha
-              <input type="text" data-testid="password-input" />
+              <input
+                type="text"
+                name="passaword"
+                data-testid="password-input"
+                onChange={ this.handleChange }
+                placeholder="Passaword"
+              />
             </label>
 
-            <button type="button"> Entrar </button>
+            <button type="button" disabled={ this.buttonLogin() }>
+              <Link to="/carteira">
+                Entrar
+              </Link>
+            </button>
           </form>
         </div>
         <footer>Desenvolvido por Leandro Lopes</footer>
