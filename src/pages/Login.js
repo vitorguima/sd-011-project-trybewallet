3b21c1cf53edfebffd7ce3dd673778d8cd3885c7
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-07-08 12:03:58 -0300

    First requirement done

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,27 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <input
+          name="user"
+          type="email"
+          data-testid="email-input"
+          placeholder="Email"
+        />
+        <input
+          style={ { display: 'flex', marginTop: '10px' } }
+          name="password"
+          type="password"
+          data-testid="password-input"
+          placeholder="Senha"
+        />
+        <button type="submit">
+          Entrar
+        </button>
+      </div>
+
+    );
   }
 }
 
