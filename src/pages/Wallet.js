289498fe7bd69f8b2abb17561bd5d28bc176ea52
Renan <renan.vamo@gmail.com>
2021-07-08 14:01:43 -0300

    add o ex5

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { email } = this.props;
+    return (
+      <>
+        <header>
+          <span data-testid="email-field">{ email }</span>
+          <span data-testid="total-field">0</span>
+          <span data-testid="header-currency-field">BRL</span>
+        </header>
+        <main>
+          Wallet
+        </main>
+      </>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+export default connect(mapStateToProps)(Wallet);
+
+Wallet.propTypes = {
+  email: PropTypes.string,
+}.isRequired;
