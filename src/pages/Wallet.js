55df5439b92a991a4b9b29368eb7b2590c0ee146
marcoswarmling <marcoswarmling15@gmail.com>
2021-07-08 16:35:25 -0300

    requisito 7

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -3,15 +3,41 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 class Wallet extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      unities: {},
+    };
+
+    this.getUnities = this.getUnities.bind(this);
+  }
+
+  async componentDidMount() {
+    this.getUnities();
+  }
+
+  async getUnities() {
+    const url = 'https://economia.awesomeapi.com.br/json/all';
+    const unities = await fetch(url)
+      .then((result) => result.json());
+    delete unities.USDT;
+    this.setState(() => ({
+      unities,
+    }));
+  }
+
   createMethods(array) {
     return array.map((term) => <option key={ term }>{ term }</option>);
   }
 
   render() {
-    const { email } = this.props;
-    const totalSpend = 0;
     const methods = ['Dinheiro', 'Cartão de crédito', 'Cartão de débito'];
     const tags = ['Alimentação', 'Lazer', 'Trabalho', 'Transporte', 'Saúde'];
+    const { email } = this.props;
+    const totalSpend = 0;
+    const { unities } = this.state;
+    const unitiesKey = Object.keys(unities);
     return (
       <div>
         <h1>TrybeWallet</h1>
@@ -21,27 +47,31 @@ class Wallet extends React.Component {
           <p data-testid="header-currency-field">BRL</p>
         </header>
         <form>
-          <label htmlFor="inp-val">
+          <label htmlFor="value">
             Valor:
-            <input type="number" name="value" id="inp-val" />
+            <input type="number" name="value" id="value" />
           </label>
-          <label htmlFor="inp-desc">
+          <label htmlFor="description">
             Descrição:
-            <input type="text" name="desc" id="inp-desc" />
+            <input type="text" name="desc" id="description" />
           </label>
-          <label htmlFor="slct-unt">
+          <label htmlFor="unity">
             Moeda:
-            <select name="unity" id="slct-unt" aria-label="Moeda:" />
+            <select name="unity" id="unity" aria-label="Moeda:">
+              {' '}
+              {this.createMethods(unitiesKey)}
+              {' '}
+            </select>
           </label>
-          <label htmlFor="slct-mthd">
+          <label htmlFor="tagSelect">
             Método de pagamento:
-            <select name="method" id="slct-mthd">
+            <select name="method" id="tagSelect">
               {this.createMethods(methods)}
             </select>
           </label>
-          <label htmlFor="slct-tag">
+          <label htmlFor="tags">
             Tag:
-            <select name="tag" id="slct-tag">
+            <select name="tag" id="tags">
               {this.createMethods(tags)}
             </select>
           </label>
