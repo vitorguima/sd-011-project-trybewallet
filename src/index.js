2acb6e00a7b190ea3ef6b2bb58164347dedc10c5
Reginara Gomes <refagomes@gmail.com>
2021-07-08 14:04:17 -0300

    tentativa de correçao de erro do lint

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import store from './store';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <Provider store={ store }>
