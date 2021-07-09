47cbaaca23379e889b01348310e5a5fe39282a17
heyset <c.inaciomatheus@gmail.com>
2021-07-09 00:01:05 -0300

    Adiciona requisito 5

--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
 import { Layout } from '../components';
 
+import withStore from '../utils/withStore';
+
 class Wallet extends React.Component {
   render() {
+    const { user } = this.props;
+
     return (
       <Layout title="Minha Carteira">
+        <header>
+          <h1>Minha carteira</h1>
+          <p>
+            Logado como:
+            <span data-testid="email-field">
+              { user.email }
+            </span>
+          </p>
+          <p>
+            Gasto total:
+            <span data-testid="total-field">
+              0
+            </span>
+          </p>
+          <p>
+            Câmbio realizado para:
+            <span data-testid="header-currency-field">
+              BRL
+            </span>
+          </p>
+        </header>
         <main>
           opa
         </main>
@@ -13,4 +38,4 @@ class Wallet extends React.Component {
   }
 }
 
-export default Wallet;
+export default withStore(Wallet, ['user']);
