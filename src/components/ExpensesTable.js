21b4595cadfc417d21321afb9f39c2d5d05c0ef5
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-07-08 23:17:03 -0300

    implemaenta o componente ExpenseTable dinamicamente a partir do store

--- src/components/ExpensesTable.js
+++ src/components/ExpensesTable.js
@@ -1,9 +1,56 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
-export default class ExpensesTable extends Component {
+class ExpensesTable extends Component {
+  renderTableRows() {
+    const { expenses } = this.props;
+    return expenses.map((item) => (
+      <tr key={ item.id }>
+        <td>{item.description}</td>
+        <td>{item.tag}</td>
+        <td>{item.method}</td>
+        <td>{item.value}</td>
+        <td>{item.exchangeRates[item.currency].name}</td>
+        <td>{parseFloat(item.exchangeRates[item.currency].ask).toFixed(2)}</td>
+        <td>
+          {parseFloat(item.value * item.exchangeRates[item.currency].ask).toFixed(2)}
+        </td>
+        <td>Real</td>
+        <td>
+          <button type="button">Editar</button>
+          <button type="button">Excluir</button>
+        </td>
+      </tr>
+    ));
+  }
+
   render() {
     return (
-      <div>TABELA</div>
+      <div>
+        <tr>
+          <th>Descrição</th>
+          <th>Tag</th>
+          <th>Método de pagamento</th>
+          <th>Valor</th>
+          <th>Moeda</th>
+          <th>Câmbio utilizado</th>
+          <th>Valor convertido</th>
+          <th>Moeda de conversão</th>
+          <th>Editar/Excluir</th>
+        </tr>
+        {this.renderTableRows()}
+      </div>
     );
   }
 }
+
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+ExpensesTable.propTypes = {
+  expenses: PropTypes.array,
+}.isRequired;
+
+export default connect(mapStateToProps)(ExpensesTable);
