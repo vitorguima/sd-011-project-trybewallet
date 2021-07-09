28ebfd16d143f37176a5ae9595fe23dc4bd27a3e
Gustavo Miyagawa <miyagawasantos@hotmail.com>
2021-07-09 02:37:46 -0400

    Requisito 5, fieto.

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Wallet extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+
   render() {
-    return <div>TrybeWallet</div>;
+    const { userEmail } = this.props;
+    return (
+      <>
+        <header>
+          <h3>Trybe Wallet</h3>
+          <p data-testid="email-field">{`Login: ${userEmail}`}</p>
+          <span data-testid="total-field">0</span>
+          <span data-testid="header-currency-field">BRL</span>
+        </header>
+        <section />
+      </>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  userEmail: state.user.email,
+});
+
+export default connect(mapStateToProps, null)(Wallet);
+
+Wallet.propTypes = {
+  userEmail: PropTypes.string.isRequired,
+};
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,7 +1,7 @@
 import { SET_USEREMAIL } from '../actions';
 
 const INITIAL_STATE = {
-  email: '',
+  email: 'teste@teste.com',
 };
 
 function user(state = INITIAL_STATE, action) {
