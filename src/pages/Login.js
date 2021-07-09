2cd4fa55b481f6090b093464bf0e2422c42c4d2d
israeltonholo <israeltonholo@hotmail.com>
2021-07-09 10:08:13 -0300

    adiciona requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import './Login.css';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div className="containnerLogin">
+        <label htmlFor="email">
+          <input
+            type="email"
+            id="email"
+            data-testid="email-input"
+            placeholder="digite seu e-mail"
+          />
+        </label>
+        <label htmlFor="password">
+          <input
+            type="password"
+            id="password"
+            data-testid="password-input"
+            placeholder="digite sua senha"
+          />
+        </label>
+        <button type="button">Entrar</button>
+      </div>
+    );
   }
 }
 
