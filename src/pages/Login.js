54196f1baef5142d12e7cfd355330e0f15ffc8cd
Vinicius <viniciusfod@gmail.com>
2021-07-09 15:06:31 -0300

    req1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,33 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <form>
+          <label htmlFor="email">
+            Email:
+            <input
+              data-testid="email-input"
+              type="email"
+              placeholder="Email"
+              name="email"
+              required
+            />
+          </label>
+          <label htmlFor="password">
+            Senha:
+            <input
+              data-testid="password-input"
+              type="password"
+              placeholder="Senha"
+              name="password"
+              required
+            />
+          </label>
+          <button type="submit">Entrar</button>
+        </form>
+      </div>
+    );
   }
 }
 
