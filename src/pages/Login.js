f2d886c728d9247f37ccc33a4b89417ff1527995
Vinicius <viniciusfod@gmail.com>
2021-07-09 16:16:49 -0300

    req-2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,31 +1,68 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+      enabledBtn: true,
+    };
+
+    this.handleUserInput = this.handleUserInput.bind(this);
+  }
+
+  checkUserData() {
+    const { email, password } = this.state;
+    const minPassword = 6;
+
+    if (email.includes('@') && email.includes('.com') && password.length >= minPassword) {
+      this.setState({
+        enabledBtn: false,
+      });
+    }
+  }
+
+  handleUserInput({ target }) {
+    const { name, value } = target;
+
+    this.setState({
+      [name]: value,
+    },
+    () => this.checkUserData());
+  }
+
   render() {
+    const { enabledBtn } = this.state;
+
     return (
       <div>
         <form>
-          <label htmlFor="email">
-            Email:
+          <fieldset>
             <input
-              data-testid="email-input"
               type="email"
-              placeholder="Email"
+              id="email-input"
               name="email"
-              required
+              data-testid="email-input"
+              placeholder="Digite seu e-mail"
+              onChange={ this.handleUserInput }
             />
-          </label>
-          <label htmlFor="password">
-            Senha:
             <input
-              data-testid="password-input"
               type="password"
-              placeholder="Senha"
+              id="password-input"
               name="password"
-              required
+              data-testid="password-input"
+              placeholder="Digite sua senha"
+              onChange={ this.handleUserInput }
             />
-          </label>
-          <button type="submit">Entrar</button>
+            <button
+              type="button"
+              disabled={ enabledBtn }
+            >
+              Entrar
+            </button>
+          </fieldset>
         </form>
       </div>
     );
