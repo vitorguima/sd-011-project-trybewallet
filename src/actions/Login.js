e22e5d3983f2aa6aeea69bc2168dec80bad52b27
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2021-07-10 13:45:20 -0300

    testando o lint

--- src/actions/Login.js
+++
@@ -1,6 +0,0 @@
-export const SET_USER_EMAIL = 'SET_USER_EMAIL';
-
-export const setUserEmail = (payload) => ({
-  type: SET_USER_EMAIL,
-  payload,
-});
--- src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-import user from './user';
-import wallet from './wallet';
-
-const reducer = combineReducers({ user, wallet });
-
-export default reducer;
