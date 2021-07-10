3126f828f63ec14e6331f828cd9455eaa2c5b413
josanjohnata <josanjohnata@gmail.com>
2021-07-09 22:28:15 -0300

    Adiciona requisito 5

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -2,8 +2,31 @@ import React from 'react';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { email } = this.props;
+    return (
+      <div>
+        <header className="headerWallet">
+          <h1>TrybeWallet</h1>
+          <h3>
+            Seja bem vindo:
+            <span data-testid="email-field">{email}</span>
+          </h3>
+          <h5 data-testid="total-field">
+            Despesa total:
+            <span data-testid="header-currency-field">BRL</span>
+          </h5>
+        </header>
+      </div>
+    );
   }
 }
 
-export default Wallet;
+const stateProps = (state) => ({
+  email: state.user.email,
+});
+
+export default connect(stateProps)(Wallet);
+
+Wallet.propTypes = {
+  email: PropTypes.string.isRequired,
+};
