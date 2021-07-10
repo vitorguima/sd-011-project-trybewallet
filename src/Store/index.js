a92b22b6e7a1c0d6d0f8a64a2fc511b08a9e65d5
Marcos Mantovani <mvmes23@gmail.com>
2021-07-09 21:08:05 -0300

    grrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr4

--- src/Store/index.js
+++
@@ -1,8 +0,0 @@
-import { applyMiddleware, createStore } from 'redux';
-import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import rootReducers from '../reducers';
-
-const store = createStore(rootReducers, composeWithDevTools(applyMiddleware(thunk)));
-
-export default store;
