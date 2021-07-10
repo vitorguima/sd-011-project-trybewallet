b6466e405a309ead4cfb60f4883dc763689dca0f
Douglas <douguinhodns@gmail.com>
2021-07-10 16:22:45 -0300

    feat: req 1

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,9 @@
 // Coloque aqui suas actions
+export const SET_EMAIL = 'SET_EMAIL';
+
+export function setEmail(payload) {
+  return {
+    type: SET_EMAIL,
+    payload,
+  };
+}
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,34 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <form>
+        <h1>Login</h1>
+        <label htmlFor="input-email">
+          Email:
+          <input
+            data-testid="email-input"
+            type="text"
+            name="email"
+            id="input-email"
+          />
+        </label>
+        <label htmlFor="input-password">
+          Senha:
+          <input
+            data-testid="password-input"
+            type="password"
+            name="senha"
+            id="input-password"
+          />
+        </label>
+        <button
+          type="button"
+        >
+          Entrar
+        </button>
+      </form>
+    );
   }
 }
 
