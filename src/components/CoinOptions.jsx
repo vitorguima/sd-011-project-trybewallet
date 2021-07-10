2303cd4dfbe1be5ee0aacf18930a1986578eacc9
Hugo Mouto <hmoutol@gmail.com>
2021-07-09 23:55:06 -0300

    fix: lint 1

--- src/components/CoinOptions.jsx
+++ src/components/CoinOptions.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
-import { useSelector } from 'react-redux';
+import React from 'react';
 
 export default function CoinOptions({ currencies }) {
   const allCurrencies = Object.keys(currencies);
-  const filteredCurrencies = allCurrencies.filter(key => key !== 'USDT' )
+  const filteredCurrencies = allCurrencies.filter((key) => key !== 'USDT');
   return (
     <>
-      {filteredCurrencies.map(key => <option key={key} value={key}>{key}</option>)}
-    </>  
+      { filteredCurrencies.map((key) => <option key={ key } value={ key }>{ key }</option>) }
+    </>
   )
-}
+};
