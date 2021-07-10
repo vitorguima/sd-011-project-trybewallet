368ab2e289c8afe30e980ecb861d770ea821c8e0
Douglas <douguinhodns@gmail.com>
2021-07-10 16:54:22 -0300

    feat: req 2 ao 4

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,46 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { setEmail } from '../actions';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      senha: '',
+      button: true,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    }, () => {
+      const { email, senha } = this.state;
+      const sizePass = 6;
+      if (/^[\w0-9.]+@\w+\.com$/.test(email) && senha.length >= sizePass) {
+        this.setState({ button: false });
+      } else {
+        this.setState({ button: true });
+      }
+    });
+  }
+
+  handleClick() {
+    const { btnClicked } = this.props;
+    const { email } = this.state;
+    btnClicked(email);
+  }
+
   render() {
+    const { button } = this.state;
     return (
       <form>
         <h1>Login</h1>
@@ -12,6 +51,7 @@ class Login extends React.Component {
             type="text"
             name="email"
             id="input-email"
+            onChange={ this.handleChange }
           />
         </label>
         <label htmlFor="input-password">
@@ -21,16 +61,29 @@ class Login extends React.Component {
             type="password"
             name="senha"
             id="input-password"
+            onChange={ this.handleChange }
           />
         </label>
-        <button
-          type="button"
-        >
-          Entrar
-        </button>
+        <Link to="/carteira">
+          <button
+            type="button"
+            disabled={ button }
+            onClick={ this.handleClick }
+          >
+            Entrar
+          </button>
+        </Link>
       </form>
     );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  btnClicked: (payloadAction) => dispatch(setEmail(payloadAction)),
+});
+
+Login.propTypes = {
+  btnClicked: PropTypes.func.isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
