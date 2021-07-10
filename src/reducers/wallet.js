144a25563cc210e5ff7422b716c1b511634d4693
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2021-07-10 08:34:34 -0300

    req 5

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,10 +1,20 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import { SET_USER_EMAIL } from '../actions/Login';
 
 const INITIAL_STATE = {
-  wallet: {
-    currencies: [],
-    expenses: [],
-  },
+  currencies: [],
+  expenses: [],
 };
 
-console.log(INITIAL_STATE);
+function wallet(state = INITIAL_STATE, action = {}) {
+  if (action.type === SET_USER_EMAIL) {
+    return ({
+      ...state,
+      currencies: [], // action.payload,
+      expenses: [], // action.payload,
+    });
+  }
+  return state;
+}
+
+export default wallet;
