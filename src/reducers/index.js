119e751f8801785269ae1edab78553b5edabcefd
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2021-07-10 15:30:04 -0300

    test

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,7 @@
 import { combineReducers } from 'redux';
 import User from './User';
 import Wallet from './Wallet';
-import CurrencyReducer from './CurrencyReducer';
 
-const reducer = combineReducers({ User, Wallet, CurrencyReducer });
+const reducer = combineReducers({ User, Wallet });
 
 export default reducer;
