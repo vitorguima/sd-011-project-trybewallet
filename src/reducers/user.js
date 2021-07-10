6ab94eb0e98384b59475cde17c5875c97c1b343a
Juliana Happatsch <jhappatsch@gmail.com>
2021-07-09 21:10:25 -0300

    req 1

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,15 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const initialState = {
+  user: {
+    email: '',
+  },
+};
+
+function user(state = initialState, action) {
+  switch (action.type) {
+  default:
+    return state;
+  }
+}
+
+export default user;
