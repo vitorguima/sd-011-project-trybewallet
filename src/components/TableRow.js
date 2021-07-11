42c50366f8e023fbf77b063a6d18e85434391ecd
PH Assis <pieit@live.com>
2021-07-10 22:53:37 -0300

    chlg11 part 1. Trying to edit the form field.

--- src/components/TableRow.js
+++ src/components/TableRow.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-lines-per-function */
 import React from 'react';
 import PropTypes from 'prop-types';
 
@@ -7,14 +8,52 @@ export default function FormInput(props) {
   const { name, ask } = exchangeRates[currency];
   const convertedPrice = ask * value;
 
+  const handleEdit = (e) => {
+    const { target } = e;
+    const Row = target.parentNode.parentNode;
+    const { childNodes } = Row;
+    const queryObj = {};
+    childNodes.forEach((el) => {
+      const obName = el.getAttribute('name');
+      const obValue = el.getAttribute('value');
+      if (obName && obValue) {
+        queryObj[obName] = obValue;
+      }
+    });
+
+    const expensesForm = document.getElementById('expensesForm');
+    document.getElementById('valueInput').value = queryObj.value;
+    document.getElementById('currency').value = queryObj.currency;
+    document.getElementById('descriptionInput').value = queryObj.description;
+    document.getElementById('paymentMethod').value = queryObj.method;
+    document.getElementById('tagInput').value = queryObj.tag;
+
+    expensesForm.style.backgroundColor = 'green';
+    expensesForm.style.color = 'white';
+
+    console.log(queryObj);
+  };
+
   return (
     <tr>
-      <td role="cell">{description}</td>
-      <td role="cell">{tag}</td>
-      <td role="cell">{method}</td>
-      <td role="cell">{value}</td>
-      <td role="cell">{name}</td>
-      <td role="cell">{parseFloat(ask).toFixed(2)}</td>
+      <td name="description" value={description} role="cell">
+        {description}
+      </td>
+      <td name="tag" value={tag} role="cell">
+        {tag}
+      </td>
+      <td name="method" value={method} role="cell">
+        {method}
+      </td>
+      <td name="value" value={value} role="cell">
+        {value}
+      </td>
+      <td name="currency" value={currency} role="cell">
+        {name}
+      </td>
+      <td name="ask" value={ask} role="cell">
+        {parseFloat(ask).toFixed(2)}
+      </td>
       <td role="cell">{convertedPrice}</td>
       <td role="cell">Real</td>
       <td>
@@ -22,12 +61,13 @@ export default function FormInput(props) {
           type="button"
           data-testid="edit-btn"
           className="btn fas fa-edit btn-info m-1"
+          onClick={(e) => handleEdit(e)}
         >
           Edit
         </button>
         <button
           type="button"
-          onClick={ () => handleDelete(id) }
+          onClick={() => handleDelete(id)}
           data-testid="delete-btn"
           className="btn fas fa-trash-alt btn-danger m-1"
         >
@@ -43,13 +83,10 @@ FormInput.propTypes = {
     description: PropTypes.string,
     method: PropTypes.string,
     tag: PropTypes.string,
-    value: PropTypes.oneOfType([
-      PropTypes.string,
-      PropTypes.number]),
+    value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
     exchangeRates: PropTypes.objectOf(PropTypes.object),
     currency: PropTypes.string,
     id: PropTypes.number,
   }).isRequired,
   handleDelete: PropTypes.func.isRequired,
-
 };
