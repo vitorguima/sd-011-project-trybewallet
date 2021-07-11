31fe48950d20846b7c4d737da1a2edaf4755a9d6
Matheus Antonio da Silva <matheusantonio232@gmail.com>
2021-07-11 18:31:19 -0300

    Requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,26 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <form>
+          <input
+            id="email"
+            data-testid="email-input"
+            type="email"
+            name="email"
+          />
+          <input
+            data-testid="password-input"
+            id="password"
+            type="password"
+            name="passwoed"
+          />
+          <button type="button">
+            Entrar
+          </button>
+        </form>
+      </div>);
   }
 }
 
