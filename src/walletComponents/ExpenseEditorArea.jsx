9e892cc740ff637b31330ae0ec89c5787b596412
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-07-11 16:05:12 -0300

    Adiciona arquivo para requisito bonus

---
+++ src/walletComponents/ExpenseEditorArea.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class ExpenseEditorArea extends Component {
+  render() {
+    return (
+      <div>
+        <p>Local onde estará toda a área para edição.</p>
+      </div>
+    );
+  }
+}
