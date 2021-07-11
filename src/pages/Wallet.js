24daf2c3bb6cb90c0f8e739e27fdf728f5038950
thosijulio <thosijulio@gmail.com>
2021-07-11 11:14:50 -0300

    resolucao exercicio 6

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import Form from '../components/Form';
 
 class Wallet extends React.Component {
   render() {
     const { emailLogin } = this.props;
     return (
-      <header>
-        <p data-testid="email-field">{ emailLogin }</p>
-        <p data-testid="total-field">0</p>
-        <p data-testid="header-currency-field">BRL</p>
-      </header>
+      <>
+        <header>
+          <p data-testid="email-field">{ emailLogin }</p>
+          <p data-testid="total-field">0</p>
+          <p data-testid="header-currency-field">BRL</p>
+        </header>
+        <Form />
+      </>
     );
   }
 }
