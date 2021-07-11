70300bffb664ee2034a3466cb91f6155ea788421
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-07-11 12:59:52 -0300

    reducers created

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,25 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+const INITIAL_STATE = {
+  currencies: [],
+  expenses: [],
+};
+
+function wallet(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case 'GET_CURRENCIES':
+    return { ...state, currencies: action.currencies };
+
+  case 'ADD_EXPENSE':
+    return { ...state, expenses: [...state.expenses, action.details] };
+
+  case 'DELETE_EXPENSE':
+    return { ...state,
+      expenses: state.expenses.filter((items) => action.id !== items.id),
+    };
+
+  default:
+    return { ...state };
+  }
+}
+
+export default wallet;
