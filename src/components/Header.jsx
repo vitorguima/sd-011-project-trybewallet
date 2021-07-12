475c68dba9b80bdc67ffd6a59a972cb9c77fe91a
Matheus Antonio da Silva <matheusantonio232@gmail.com>
2021-07-12 00:54:08 -0300

    Requisito 5

---
+++ src/components/Header.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import '../assets/header.css';
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: 0,
+      currency: 'BRL',
+    };
+  }
+
+  render() {
+    const { email } = this.props;
+    const { total, currency } = this.state;
+    return (
+      <div className="header">
+        <h1>TrybeWallet</h1>
+        <div className="user-info">
+          <div className="user-info-container">
+            <p data-testid="email-field">{email}</p>
+          </div>
+          <div className="user-info-container">
+            <p>
+              Dispesa Total:
+              <span data-testid="total-field">{total}</span>
+              <span data-testid="header-currency-field">{currency}</span>
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Header);
