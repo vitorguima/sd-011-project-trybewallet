6cd14a4f73c215afd9d41cdf5670a289c7f8e7fe
Lucas Maestrelli <lucasmaestrelli95@gmail.com>
2021-07-11 21:07:48 -0300

    estruturaçao + req 1 a 4

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,91 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { sendEmail } from '../actions';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+      disabled: true,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.validate = this.validate.bind(this);
+  }
+
+  componentDidUpdate() {
+    this.validate();
+  }
+
+  validate() {
+    const { email, password, disabled } = this.state;
+    const number = 6;
+    const verifyEmail = email.split('').includes('@') && email.split('.').includes('com');
+    const verifyPassword = password.length >= number;
+    if (verifyEmail && verifyPassword && disabled) {
+      this.setState({ disabled: false });
+    } else if ((!verifyEmail || !verifyPassword) && !disabled) {
+      this.setState({ disabled: true });
+    }
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password, disabled } = this.state;
+    const { dispatchSendEmail } = this.props;
+    return (
+      <div>
+        <label htmlFor="email">
+          Insira o E-mail:
+          <input
+            type="email"
+            name="email"
+            value={ email }
+            onChange={ this.handleChange }
+            data-testid="email-input"
+            placeholder="nome@provedor.com"
+          />
+        </label>
+        <label htmlFor="password">
+          Insira a senha:
+          <input
+            type="password"
+            name="password"
+            value={ password }
+            onChange={ this.handleChange }
+            data-testid="password-input"
+            placeholder="Senha"
+          />
+        </label>
+        <Link to="/carteira">
+          <button
+            type="button"
+            disabled={ disabled }
+            onClick={ () => dispatchSendEmail(email) }
+          >
+            Entrar
+          </button>
+        </Link>
+      </div>
+    );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  dispatchSendEmail: (email) => dispatch(sendEmail(email)),
+});
+
+Login.propTypes = {
+  dispatchSendEmail: PropTypes.func,
+}.isRequired;
+
+export default connect(null, mapDispatchToProps)(Login);
