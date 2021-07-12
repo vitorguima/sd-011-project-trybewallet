c561696a6d8b9336623ed930e321b472563cae91
Gustavo Mendes <gusm217@gmail.com>
2021-07-12 11:51:01 -0300

    Adiciona o 4 e 5

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,45 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import '../App.css';
 
 class Wallet extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      gastos: 0,
+      cambio: 'BRL',
+    };
+  }
+
   render() {
-    return <div>TrybeWallet</div>;
+    const { userEmail } = this.props;
+    const { gastos, cambio } = this.state;
+    return (
+      <div className="wallet-container">
+        <section>
+          <img
+            className="trybe-img"
+            src="/public/v7oifu5eirdxepzyg0p0.png"
+            alt="símbolo Trybe"
+          />
+        </section>
+        <section>
+          <span data-testid="email-field">{userEmail}</span>
+          <span data-testid="total-field">{gastos}</span>
+          <span data-testid="header-currency-field">{cambio}</span>
+        </section>
+      </div>);
   }
 }
 
-export default Wallet;
+Wallet.propTypes = {
+  userEmail: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  userEmail: state.user.email,
+});
+
+export default connect(mapStateToProps)(Wallet);
