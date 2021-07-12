66bcea5cecb798fea64a92e92e79e8f872dcbcda
thosijulio <thosijulio@gmail.com>
2021-07-12 14:06:51 -0300

    resolucao exercicio 9

---
+++ src/components/TableExpense.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+class TableExpense extends React.Component {
+  render() {
+    const { expenses } = this.props;
+    console.log(expenses);
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          { expenses.map((expense, key) => (
+            <tr key={ key }>
+              <td>{ expense.description }</td>
+              <td>{ expense.tag }</td>
+              <td>{ expense.method }</td>
+              <td>{ expense.value }</td>
+              <td>{ (expense.exchangeRates[expense.currency].name).split('/', 1) }</td>
+              <td>
+                {
+                  parseFloat(expense.exchangeRates[expense.currency].ask)
+                    .toFixed(2)
+                }
+              </td>
+              <td>
+                {
+                  (expense
+                    .exchangeRates[expense.currency]
+                    .ask * parseFloat(expense.value))
+                    .toFixed(2)
+                }
+              </td>
+              <td>Real</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+TableExpense.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    value: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.number,
+    ]).isRequired,
+    description: PropTypes.string.isRequired,
+    currency: PropTypes.string.isRequired,
+    method: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+  })).isRequired,
+};
+
+export default connect(mapStateToProps)(TableExpense);
