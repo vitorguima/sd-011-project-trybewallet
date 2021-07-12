548ff8b038ba927956587c0cebb52c78ce6b05f5
Savio Moraes <smmsavio@hotmail.com>
2021-07-11 22:25:47 -0300

    Corrige linter, Requisito 04 passando

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,17 +1,16 @@
-// import { SET_EMAIL } from '../actions/index';
+import { SET_EMAIL } from '../actions/index';
 
 const INITIAL_STATE = {
   email: '',
 };
 
-function user(state = INITIAL_STATE, _action) {
-  // switch (action.type) {
-  // case SET_EMAIL:
-  //   return { ...state, email: action.payload };
-  // default:
-  //   return state;
-  // }
-  return state;
+function user(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case SET_EMAIL:
+    return { ...state, email: action.payload };
+  default:
+    return state;
+  }
 }
 
 export default user;
