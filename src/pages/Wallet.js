87db782e898237b6f32fcaa807725e1ffb5592f4
Jessica Arruda <sicarruda@gmail.com>
2021-07-11 21:40:36 -0300

    resoluçao requisito 5

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,60 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { email } = this.props;
+    return (
+      <div>
+        <header>
+          <p data-testid="email-field">{email}</p>
+          <p data-testid="total-field"> 0 </p>
+          <p data-testid="header-currency-field"> BRL </p>
+        </header>
+        <form>
+          <label htmlFor="input-value">
+            Valor
+            <input id="input-value" type="text" />
+          </label>
+
+          <label htmlFor="description">
+            Descrição
+            <input id="description" type="text" />
+          </label>
+          {/* <label htmlFor="currency">
+            Moeda
+            <select id="currency" />
+          </label> */}
+          <label htmlFor="payment-method">
+            Metodo de Pagamento
+            <select id="payment-method">
+              <option value="money"> Dinheiro</option>
+              <option value="credit-card">Cartão de crédito</option>
+              <option value="debit-card">Cartão de débito</option>
+            </select>
+          </label>
+          <label htmlFor="tag">
+            <select id="tag">
+              <option value="food">Alimentação</option>
+              <option value="laze">Lazer</option>
+              <option value="work">Trabalho</option>
+              <option value="Transport">Transporte</option>
+              <option value="health">Saúde</option>
+            </select>
+          </label>
+        </form>
+      </div>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+Wallet.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Wallet);
