8d2f9befac2208bed17d0e2e8d2024161067ccfb
Alan Gonçalves <vekio.soft@gmail.com>
2021-07-12 09:35:40 -0300

    cria reducer user.js

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,15 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_STATE = {
+  username: '',
+};
+
+function user(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case 'ACCESS_USER':
+    return state;
+  default:
+    return state;
+  }
+}
+
+export default user;
