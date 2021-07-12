437b7e173f8b9c38204a9674d3c8da0fde7a6327
Alan Gonçalves <vekio.soft@gmail.com>
2021-07-12 09:47:30 -0300

    configurando Redux DevTools

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,8 @@
-import { createStore, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 
-const extension = window.devToolsExtension() || ((f) => f);
-const store = createStore(rootReducer, compose(extension));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 export default store;
